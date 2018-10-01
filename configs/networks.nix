efeb5bf2711be2ee16daa30d43f5cc62d6bb3b7b
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-10-01 08:26:07 +0200

    networks: add eduroam, add secret file

--- configs/networks.nix
+++ configs/networks.nix
@@ -1,12 +1,28 @@
-{
+let eduroam = (import ../secrets.nix).eduroam;
+in {
   networking.hosts = {
     "192.168.178.27" = [ "printer.local" ];
   };
 
   networking.wireless.enable = true;
+  networking.wireless.userControlled.enable = true;
   networking.wireless.networks = {
     Aether = { pskRaw = "e1b18af54036c5c9a747fe681c6a694636d60a5f8450f7dec0d76bc93e2ec85a"; };
     "Asoziales Netzwerk" = { pskRaw = "8e234041ec5f0cd1b6a14e9adeee9840ed51b2f18856a52137485523e46b0cb6"; };
     "c-base-public" = {};
+    "FlixBus" = {};
+    "FlixBus Wi-Fi" = {};
+    eduroam = {
+      auth = ''
+        key_mgmt=WPA-EAP
+        eap=TTLS
+        proto=RSN
+        identity="${eduroam.identity}"
+        anonymous_identity="anonymous@cms.hu-berlin.de"
+        password="${eduroam.password}"
+        ca_cert="${builtins.fetchurl https://www.cms.hu-berlin.de/de/dl/netze/wlan/config/eduroam/deutsche-telekom-root-ca-2.crt}"
+        phase2="auth=PAP"
+      '';
+    };
   };
 }
