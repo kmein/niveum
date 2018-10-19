82a98fb0720aa8a50cd16e0b3612c304391468ff
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-10-19 21:38:43 +0200

    SSH config
    + github, gitlab, happysrv

--- dot/ssh.nix
+++ dot/ssh.nix
@@ -2,7 +2,20 @@
 with lib;
 let huServer = name: ''
   Host ${name}
-      HostName ${name}.informatik.hu-berlin.de
       User ${(import ../secrets.nix).eduroam.identity}
+      HostName ${name}.informatik.hu-berlin.de
   '';
-in strings.concatMapStringsSep "\n\n" huServer [ "rabe" "star" "gruenau" "gruenau1" "gruenau2" "gruenau3" "gruenau4" "gruenau5" "gruenau6" "gruenau7" "gruenau8" ]
+in ''
+  Host github
+      User git
+      HostName github.com
+
+  Host gitlab
+      User git
+      HostName gitlab.informatik.hu-berlin.de
+
+  Host happysrv
+      User meinhark
+      HostName v22017123717458308.happysrv.de
+      Port 49000
+'' + strings.concatMapStringsSep "\n\n" huServer [ "rabe" "star" "gruenau" "gruenau1" "gruenau2" "gruenau3" "gruenau4" "gruenau5" "gruenau6" "gruenau7" "gruenau8" ]
