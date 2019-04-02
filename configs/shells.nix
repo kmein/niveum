01d6c517f7d5b00d0166effc7ec970fc5533435c
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-04-03 00:31:55 +0200

    zsh: completion from omz

--- configs/shells.nix
+++ configs/shells.nix
@@ -53,7 +53,19 @@ in {
       setopt AUTOCD CDABLE_VARS
       setopt HIST_IGNORE_ALL_DUPS
       setopt VI
+      setopt AUTO_MENU
+      setopt COMPLETE_IN_WORD
+      setopt ALWAYS_TO_END
       unsetopt NOMATCH
+      unsetopt MENU_COMPLETE
+
+      zstyle ':completion:*:*:*:*:*' menu select
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+      zstyle ':completion:*' special-dirs true
+      zstyle ':completion:*' list-colors \'\'
+      zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+      zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+      zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
       export KEYTIMEOUT=1
 
