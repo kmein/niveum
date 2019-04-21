83df0232c684f3c3000c9572712c90bee5fcc3e5
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-04-21 08:31:59 +0200

    catullus: use packageOverrides

--- systems/catullus/containers.nix
+++ systems/catullus/containers.nix
@@ -1,10 +1,15 @@
 { config, pkgs, lib, ... }:
 let
-  telegram-reverse = pkgs.python3Packages.callPackage <packages/telegram-reverse.nix> {};
-  telegram-betacode = pkgs.python3Packages.callPackage <packages/telegram-betacode.nix> {};
   # telegram-horoscope = pkgs.python3Packages.callPackage <packages/telegram-horoscope.nix> {};
-  telegram-proverb = pkgs.python3Packages.callPackage <packages/telegram-proverb.nix> {};
 in {
+  nixpkgs.config.packageOverrides = pkgs: {
+    autorenkalender = pkgs.callPackage <packages/autorenkalender.nix> {};
+    literature-quote = pkgs.callPackage <packages/literature-quote.nix> {};
+    telegram-proverb = pkgs.python3Packages.callPackage <packages/telegram-proverb.nix> {};
+    telegram-betacode = pkgs.python3Packages.callPackage <packages/telegram-betacode.nix> {};
+    telegram-reverse = pkgs.python3Packages.callPackage <packages/telegram-reverse.nix> {};
+  };
+
   niveum.telegramBots.quotebot = {
     enable = true;
     time = "08:00";
@@ -27,7 +32,7 @@ in {
     description = "Telegram bot for reversing things";
     environment.TELEGRAM_REVERSE_TOKEN = builtins.readFile <secrets/telegram-reverse.token>;
     enable = true;
-    script = ''${telegram-reverse}/bin/telegram-reverse'';
+    script = ''${pkgs.telegram-reverse}/bin/telegram-reverse'';
     serviceConfig.Restart = "always";
   };
 
@@ -36,7 +41,7 @@ in {
     description = "Telegram bot for converting Ancient Greek betacode into unicode";
     environment.TELEGRAM_BETACODE_TOKEN = builtins.readFile <secrets/telegram-betacode.token>;
     enable = true;
-    script = ''${telegram-betacode}/bin/telegram-betacode'';
+    script = ''${pkgs.telegram-betacode}/bin/telegram-betacode'';
     serviceConfig.Restart = "always";
   };
 
@@ -45,7 +50,7 @@ in {
     description = "Telegram bot for generating inspiring but useless proverbs";
     environment.TELEGRAM_PROVERB_TOKEN = builtins.readFile <secrets/telegram-proverb.token>;
     enable = true;
-    script = ''${telegram-proverb}/bin/proverb_bot.py'';
+    script = ''${pkgs.telegram-proverb}/bin/proverb_bot.py'';
     serviceConfig.Restart = "always";
   };
   # systemd.services.telegram-horoscope = {
