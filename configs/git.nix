47a7a9f7fb350739f46f293155477a73b09d3961
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-05-31 02:16:43 +0200

    + git graph

--- configs/git.nix
+++ configs/git.nix
@@ -38,6 +38,7 @@
       diffs = "diff --staged";
       last = "log -1 HEAD";
       logs = "log --pretty=oneline";
+      graph = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
     };
     ignores = config.niveum.ignore;
   };
