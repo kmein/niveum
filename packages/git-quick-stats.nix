4a46aad1c82b96add8c774a2e867d39617938dd7
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-06-05 19:37:13 +0200

    + virtualenv, ~ git-quick-stats

--- packages/git-quick-stats.nix
+++ packages/git-quick-stats.nix
@@ -1,14 +1,21 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation rec {
-  name = "git-quick-stats";
+  pname = "git-quick-stats";
   version = "2.0.8";
   src = fetchFromGitHub {
-    repo = name;
+    repo = "git-quick-stats";
     owner = "arzzen";
-    rev = "c11bce17bdb1c7f1272c556605b48770646bf807";
+    rev = "${version}";
     sha256 = "1px1sk7b6mjnbclsr1jn33m9k4wd8wqyw4d6w1rgj0ii29lhzmqi";
   };
   installPhase = ''
     PREFIX=$out make install
   '';
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/arzzen/git-quick-stats";
+    description = "Git quick statistics is a simple and efficient way to access various statistics in git repository.";
+    platforms = platforms.all;
+    maintainers = [ maintainers.kmein ];
+    license = licenses.mit;
+  };
 }
