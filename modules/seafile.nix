f19bc1b64c4c9db49504701350936eaa545f2d82
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-06-08 14:57:22 +0200

    ~ seafile with systemd

--- modules/seafile.nix
+++ modules/seafile.nix
@@ -4,10 +4,19 @@ let cfg = config.niveum.seafile;
 in {
   options.niveum.seafile = {
     enable = mkEnableOption "Seafile";
+    user = mkOption { type = types.attrs; };
   };
 
   config = lib.mkIf cfg.enable {
-    services.xserver.displayManager.sessionCommands = "${pkgs.seafile-client}/bin/seafile-applet &";
+    systemd.services.seafile = {
+      description = "Seafile synchronisation service";
+      after = [ "network-online.target" ];
+      script = "${pkgs.seafile-client}/bin/seafile-applet";
+      serviceConfig = {
+        Restart = "always";
+        User = cfg.user.name;
+      };
+    };
 
     environment.systemPackages = [ pkgs.seafile-client pkgs.seafile-shared ];
   };
