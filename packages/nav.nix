a87cb00b51c97e33c1735a5df3bd87eebfd85d63
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-07-03 20:43:55 +0200

    + nav

---
+++ packages/nav.nix
@@ -0,0 +1,21 @@
+{ fetchFromGitHub, stdenv, fzf }:
+stdenv.mkDerivation rec {
+  name = "nav";
+  version = "8da22b1";
+  src = fetchFromGitHub {
+    owner = "huntrar";
+    repo = name;
+    rev = version;
+    sha256 = "0aw10495901dagyfxn2pj6nh3nl1xgi5p57mwgkgn9g5hi66xa87";
+  };
+  installPhase = ''
+    mkdir -p $out/bin/
+    install nav $out/bin/
+  '';
+  propagatedBuildInputs = [ fzf ];
+  meta = {
+    maintainer = [ stdenv.lib.maintainers.kmein ];
+    description = "JSON data structure navigator";
+    homepage = "https://github.com/huntrar/nav";
+  };
+}
