53ebb00d8eda351db0b2226a1d6486467f566711
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-07-03 21:45:35 +0200

    hledger: add LEDGER_FILE option

--- configs/hledger.nix
+++ configs/hledger.nix
@@ -4,8 +4,9 @@
 
   niveum.hledger = {
     enable = true;
+    ledgerFile = "$HOME/prog/git/ledger/current.journal";
     server = {
-      enable = true;
+      enable = false;
       user = config.users.users.me;
       package = pkgs.unstable.hledger-web;
     };
