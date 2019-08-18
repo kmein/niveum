df7439fd5701c3126ffdf144c2c0174fae5c3e0b
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-08-18 19:15:54 +0200

    scripts: fix syntax

--- packages/libcoap.nix
+++ packages/libcoap.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation {
     repo = "libcoap";
     owner = "obgm";
     rev = "25863042ae1e95138776f65c772f9fa6ce60495c";
-    fetchSubmodules = true;
     sha256 = "1nic584jwkndg0w831h0fnxk0zx0apf2lw5md079m3di7zcxs6bw";
+    fetchSubmodules = true;
   };
   buildInputs = [
     automake
@@ -14,13 +14,13 @@ stdenv.mkDerivation {
     which
     libtool
   ];
-  preConfigure = ''./autogen.sh'';
+  preConfigure = "./autogen.sh";
   configureFlags = [
     "--disable-documentation"
     "--disable-shared"
   ];
   meta = with stdenv.lib; {
-    homepage = https://github.com/obgm/libcoap;
+    homepage = "https://github.com/obgm/libcoap";
     description = "A CoAP (RFC 7252) implementation in C";
     platforms = platforms.linux;
     license = licenses.bsd2;
