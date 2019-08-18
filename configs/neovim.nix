94f9449f6cf3aa223331614c4807f1434f4722f5
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-08-18 19:15:18 +0200

    vim: add todo.txt

--- configs/neovim.nix
+++ configs/neovim.nix
@@ -69,6 +69,15 @@
             vimtex
             vim-ledger
             (pkgs.vimUtils.buildVimPluginFrom2Nix {
+              name = "todo.txt-vim";
+              src = pkgs.fetchFromGitHub {
+                owner = "freitass";
+                repo = "todo.txt-vim";
+                rev = "6845221d45bd62e604c2024bc511a56e79d1118b";
+                sha256 = "08m9q5f2pz6gjp0vkmm7glfsrbnldxi1j59dm5d7any6y96xxd6v";
+              };
+            })
+            (pkgs.vimUtils.buildVimPluginFrom2Nix {
               name = "jq.vim";
               src = pkgs.fetchFromGitHub {
                 owner = "vito-c";
