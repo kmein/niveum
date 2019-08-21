a4e67e08693e53e21548b2a2bdb3593e0a1930d0
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-08-21 09:13:00 +0200

    add tentative minecraft module

---
+++ modules/minecraft.nix
@@ -0,0 +1,16 @@
+{ pkgs, lib, config, ... }:
+with lib;
+let
+  cfg = config.niveum.minecraft;
+in
+{
+  options.niveum.minecraft = {
+    enable = mkEnableOption "Minecraft";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      pkgs.minecraft
+    ];
+  };
+}
