f93a0e04a261e2164aa5711df7d6400e458e8d8b
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-09-25 18:37:08 +0200

    fix(sncli): use toINI

--- configs/sncli.nix
+++ configs/sncli.nix
@@ -1,13 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let secrets = import <dot/secrets.nix>;
 in {
   environment.systemPackages = [ pkgs.python3Packages.sncli ];
 
   home-manager.users.me = {
-    home.file.".snclirc".text = ''
-      [sncli]
-      cfg_sn_username = ${secrets.simplenote.username}
-      cfg_sn_password = ${secrets.simplenote.password}
-    '';
+    home.file.".snclirc".text = lib.generators.toINI {} {
+      sncli = {
+        cfg_sn_username = secrets.simplenote.username;
+        cfg_sn_password = secrets.simplenote.password;
+      };
+    };
   };
 }
