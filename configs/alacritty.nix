93c87be579000989d035945cf4e712cf76a54bd9
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-09-25 10:10:36 +0200

    fix: update alacritty

--- configs/alacritty.nix
+++ configs/alacritty.nix
@@ -3,8 +3,8 @@
   niveum.applications.terminal = "alacritty";
 
   environment.systemPackages = [
-    pkgs.alacritty
-    pkgs.alacritty.terminfo
+    pkgs.unstable.alacritty
+    pkgs.unstable.alacritty.terminfo
   ];
 
   home-manager.users.me.xdg.configFile."alacritty/alacritty.yml".text =
@@ -15,14 +15,13 @@
     colors = {
       primary = {
         inherit (config.niveum.colours) background foreground;
-        dim_foreground = config.niveum.colours.background;
       };
       normal = lib.mapAttrs (_: colour: colour.dark) colours;
       bright = lib.mapAttrs (_: colour: colour.bright) colours;
     };
     font = {
-      normal.family = config.niveum.fonts.terminal.name;
-      size = config.niveum.fonts.terminal.size - 2;
+      normal.family = "Monospace";
+      size = config.niveum.fonts.size - 2;
     };
     key_bindings = [
       { key = "Add"; mods = "Control"; action = "IncreaseFontSize"; }
