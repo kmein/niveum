4049e2b905339225a0d4bb525ddf2cf47db49dda
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-10-24 09:07:49 +0200

    fix(syncthing): use new declarative options

--- configs/cloud.nix
+++ configs/cloud.nix
@@ -4,7 +4,6 @@
     <modules/seafile.nix>
     <modules/google-drive.nix>
     <modules/dropbox.nix>
-    <stockholm/krebs/3modules/syncthing.nix>
   ];
 
   niveum.dropbox = {
@@ -29,20 +28,20 @@
     openDefaultPorts = true;
     configDir = "/home/kfm/.config/syncthing";
     dataDir = "/home/kfm/.config/syncthing";
-  };
-
-  krebs.syncthing = rec {
-    enable = true;
-    key = toString <secrets/syncthing/key.pem>;
-    cert = toString <secrets/syncthing/cert.pem>;
-    peers = {
-      homeros.id = "HSOL72W-MMN346W-C3WCWCH-OTRKJYG-MY2WWV6-P7JUHN7-5WYYYRV-ZMH4KAA";
-      scardanelli.id = "7CZYHEX-3CSFDQU-PEEMYHG-6XGQ2ZD-KGVUWH5-GFRB2XK-FP57ERX-7APZUQU";
-      rilke.id = "NYNNHXP-7JMSTXG-SVNOPWD-RWXCCCL-CBOVBEI-X4QPLF4-NJA5G2P-RSGYRQQ";
-      wilde.id = "R6DEBD7-G5RYDKN-VFA3HPO-WX4DNVI-373F7OQ-AW5MZTT-3L4BDVW-Y6ROEAF";
+    declarative = rec {
+      cert = toString <secrets/syncthing/cert.pem>;
+      key = toString <secrets/syncthing/key.pem>;
+      devices = {
+        homeros.id = "HSOL72W-MMN346W-C3WCWCH-OTRKJYG-MY2WWV6-P7JUHN7-5WYYYRV-ZMH4KAA";
+        scardanelli.id = "7CZYHEX-3CSFDQU-PEEMYHG-6XGQ2ZD-KGVUWH5-GFRB2XK-FP57ERX-7APZUQU";
+        rilke.id = "NYNNHXP-7JMSTXG-SVNOPWD-RWXCCCL-CBOVBEI-X4QPLF4-NJA5G2P-RSGYRQQ";
+        wilde.id = "R6DEBD7-G5RYDKN-VFA3HPO-WX4DNVI-373F7OQ-AW5MZTT-3L4BDVW-Y6ROEAF";
+      };
+      folders = let syncthing-dir = "${config.users.users.me.home}/cloud/syncthing"; in {
+        "${syncthing-dir}/common".devices = [ "homeros" "scardanelli" "wilde" ];
+        "${syncthing-dir}/library".devices = lib.attrNames devices;
+        "${syncthing-dir}/mundoiu".devices = lib.attrNames devices;
+      };
     };
-    folders."${config.users.users.me.home}/cloud/syncthing/common".peers = [ "homeros" "scardanelli" "wilde" ];
-    folders."${config.users.users.me.home}/cloud/syncthing/library".peers = lib.attrNames peers;
-    folders."${config.users.users.me.home}/cloud/syncthing/mundoiu".peers = lib.attrNames peers;
   };
 }
