15cddf337e552491bb9ca8c8a13d6832c7a15a84
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-10-26 13:30:12 +0200

    feat(printing): try to add OfficeJet

--- configs/printing.nix
+++ configs/printing.nix
@@ -5,5 +5,26 @@
     drivers = [ pkgs.hplipWithPlugin ];
   };
 
-  networking.hosts."192.168.178.27" = [ "officejet" ];
+  # networking.hosts."192.168.178.27" = [ "officejet" ];
+
+  hardware.printers.ensurePrinters = [
+    {
+      name = "OfficeJet";
+      location = "Living room";
+      deviceUri = "https://192.168.178.27";
+      model = "drv:///hp/hpcups.drv/hp-officejet_4650_series.ppd";
+      ppdOptions = {
+        Duplex = "DuplexNoTumble"; # DuplexNoTumble DuplexTumble None
+        PageSize = "A4"; # A4 A4.FB A4.Duplex
+        MediaType = "Plain";
+        OutputMode = "Normal";
+        ColorModel = "KGray";  # RGB CMYGray KGray
+      };
+    }
+  ];
 }
+
+/*
+HP/hp-officejet_4650_series.ppd.gz
+drv:///hp/hpcups.drv/hp-officejet_4650_series.ppd
+*/
