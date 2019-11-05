ce100eacbba814e863058ed3f50eae799c256557
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-11-05 08:38:48 +0100

    fix: readd latex, take pandoc from unstable

--- configs/packages/writing.nix
+++ configs/packages/writing.nix
@@ -1,15 +1,11 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # (texlive.combine {
-    #   inherit (pkgs.texlive) scheme-full texdoc latex2e-help-texinfo;
-    #   pkgFilter = pkg: pkg.tlType == "run" || pkg.tlType == "bin" || pkg.pname == "latex2e-help-texinfo";
-    # })
-    # haskellPackages.patat
+    texlive.combined.scheme-full
     (aspellWithDicts (dict: [dict.de dict.en dict.la dict.en-computers dict.ru]))
-    haskellPackages.pandoc-citeproc
+    unstable.haskellPackages.pandoc-citeproc
     libreoffice
-    pandoc
+    unstable.pandoc
     proselint
     unstable.asciidoctor
     wordnet
