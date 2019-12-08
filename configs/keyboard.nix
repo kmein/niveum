ac298b850ef4a86fad7b00ee71bc9ed36d00debf
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-12-08 22:02:14 +0100

    fix(keyboard): remove russian

--- configs/keyboard.nix
+++ configs/keyboard.nix
@@ -2,8 +2,8 @@
 let inherit (import <niveum/lib> { inherit lib; }) commaSep;
 in {
   services.xserver = {
-    layout = commaSep [ "de" "gr" "ru" ];
-    xkbVariant = commaSep [ "T3" "polytonic" "phonetic_winkeys" ];
+    layout = commaSep [ "de" "gr" ];
+    xkbVariant = commaSep [ "T3" "polytonic" ];
     xkbOptions = commaSep [ "compose:caps" "terminate:ctrl_alt_bksp" "grp:ctrls_toggle" ];
     libinput.enable = true;
   };
