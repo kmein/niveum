a77e36c0f607f5c94eea3748a1b26e91610ca12a
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-01-03 12:01:32 +0100

    fix(todo.txt): correctly set TODO_DIR

--- configs/todo-txt.nix
+++ configs/todo-txt.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
-let todo-txt-config = pkgs.writeText "todo.cfg" ''
-  export TODO_DIR="$(echo "$(${pkgs.git}/bin/git rev-parse --show-toplevel 2>/dev/null)/.todo" || echo "$HOME/cloud/Dropbox/todo")"
+let
+  get-todo-dir = pkgs.writers.writeDash "git-toplevel-todo" ''
+    if GIT_TOPLEVEL=$(${pkgs.git}/bin/git rev-parse --show-toplevel); then
+      echo "$GIT_TOPLEVEL/.todo"
+    else
+      echo "$HOME/cloud/Dropbox/todo"
+    fi
+  '';
+  todo-txt-config = pkgs.writeText "todo.cfg" ''
+    export TODO_DIR="$(${get-todo-dir})"
 
-  export TODO_FILE="$TODO_DIR/todo.txt"
-  export DONE_FILE="$TODO_DIR/done.txt"
-  export REPORT_FILE="$TODO_DIR/report.txt"
-'';
+    export TODO_FILE="$TODO_DIR/todo.txt"
+    export DONE_FILE="$TODO_DIR/done.txt"
+    export REPORT_FILE="$TODO_DIR/report.txt"
+  '';
 in {
   environment = {
     systemPackages = [ pkgs.todo-txt-cli ];
