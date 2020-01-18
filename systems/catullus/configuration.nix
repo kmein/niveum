7c19c3b4f0824f059edad0711252cbe1b1fdff50
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-01-18 17:10:33 +0100

    fix(catullus): disable wireless

--- systems/catullus/configuration.nix
+++ systems/catullus/configuration.nix
@@ -149,7 +149,7 @@ in {
   time.timeZone = "Europe/Berlin";
 
   networking.wireless = {
-    enable = true;
+    enable = false;
     networks.Aether = { pskRaw = "e1b18af54036c5c9a747fe681c6a694636d60a5f8450f7dec0d76bc93e2ec85a"; };
   };
 
