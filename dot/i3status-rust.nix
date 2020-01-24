d7aca4feb609c3c2dda57ed91b48e95c563ae704
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-01-24 10:09:04 +0100

    feat(i3 status rust)

--- dot/i3status-rust.nix
+++ dot/i3status-rust.nix
@@ -3,12 +3,10 @@
   theme = {
     name = "plain";
     overrides = {
-      separator = " ";
       critical_fg = colours.red.bright;
       good_fg = colours.green.bright;
       idle_fg = colours.foreground;
       info_fg = colours.foreground;
-      separator_fg = "auto";
       warning_fg = colours.yellow.bright;
       alternating_tint_bg = colours.background;
       alternating_tint_fg = colours.background;
@@ -16,31 +14,10 @@
       good_bg = colours.background;
       idle_bg = colours.background;
       info_bg = colours.background;
-      separator_bg = "auto";
       warning_bg = colours.background;
     };
   };
-  icons = {
-    name = "none";
-    overrides = {
-      music = "♫ ";
-      music_play = "►";
-      music_next = "→";
-      music_prev = "←";
-      volume_full = "♪ ";
-      volume_half = "♪ ";
-      volume_empty = "♪ ";
-      bat =  "⚡ ";
-      bat_full = "⚡ ";
-      bat_charging = "↑ ";
-      bat_discharging = "↓ ";
-      memory_mem = "";
-      separator = "";
-      net_wireless = "";
-      cogs = "";
-      time = "";
-    };
-  };
+  icons.name = "awesome";
   block = [
     {
       block = "music";
@@ -48,14 +25,15 @@
       buttons = ["prev" "play" "next"];
       marquee = false;
       max_width = 35;
+      on_collapsed_click = "spotify";
     }
     {
       block = "net";
       device = wifi-interface;
-      ssid = true;
-      interval = 1;
       speed_up = false;
       speed_down = false;
+      signal_strength = true;
+      ssid = true;
     }
     {
       block = "battery";
@@ -64,6 +42,10 @@
     }
     {
       block = "sound";
+      on_click = "pavucontrol";
+    }
+    {
+      block = "disk_space";
     }
     {
       block = "memory";
@@ -73,8 +55,6 @@
     }
     {
       block = "load";
-      interval = 1;
-      format = "{1m}";
     }
     {
       block = "time";
