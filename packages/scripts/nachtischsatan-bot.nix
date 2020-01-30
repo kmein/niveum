8a15de75f30a55cda820a4f4b00221a175af61ac
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-01-30 19:09:58 +0100

    fix(nachtischsatan-bot): sleep for up to 4 seconds

--- packages/scripts/nachtischsatan-bot.nix
+++ packages/scripts/nachtischsatan-bot.nix
@@ -1,10 +1,14 @@
 { token, writers, python3Packages, ... }:
 writers.writePython3 "nachtischsatan-bot" { libraries = [ python3Packages.python-telegram-bot ]; } ''
+  import random
+  import time
+
   from telegram.ext import Updater, MessageHandler
   from telegram.ext.filters import Filters
 
 
   def flubber(bot, update):
+      time.sleep(random.randrange(4000) / 1000)
       update.message.reply_text("*flubberflubber*")
 
 
