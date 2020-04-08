d3b58a3053274f9bf2577ae9c495bea22b0ecf3b
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-04-08 18:27:27 +0200

    feat(syncthing): add heym

--- configs/cloud.nix
+++ configs/cloud.nix
@@ -29,6 +29,7 @@
         scardanelli.id = "7CZYHEX-3CSFDQU-PEEMYHG-6XGQ2ZD-KGVUWH5-GFRB2XK-FP57ERX-7APZUQU";
         rilke.id = "NYNNHXP-7JMSTXG-SVNOPWD-RWXCCCL-CBOVBEI-X4QPLF4-NJA5G2P-RSGYRQQ";
         wilde.id = "R6DEBD7-G5RYDKN-VFA3HPO-WX4DNVI-373F7OQ-AW5MZTT-3L4BDVW-Y6ROEAF";
+        heym.id = "HLQSG3D-WSKLA6S-MEYQ3EU-GDBGABE-PY53RQ6-SWQAP2I-Z5MVBVX-MYPJXAM";
       };
       folders = let syncthing-dir = "${config.users.users.me.home}/cloud/syncthing"; in {
         "${syncthing-dir}/common".devices = [ "homeros" "scardanelli" "wilde" ];
