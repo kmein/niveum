8d1e7238cf1a27a7c548e34ae790c98f84c9846c
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-04-09 18:15:41 +0200

    fix(packages): reorganize nix-git

--- packages/nix-git.nix
+++
@@ -1,4 +0,0 @@
-{ writeShellScriptBin, nix-prefetch-git, jq }:
-writeShellScriptBin "nix-git" ''
-  ${nix-prefetch-git}/bin/nix-prefetch-git "$@" 2> /dev/null | ${jq}/bin/jq -r '"rev = \"\(.rev)\";\nsha256 = \"\(.sha256)\";"'
-''
