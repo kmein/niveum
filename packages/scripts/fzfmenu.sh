09e991d87eed2a7627925cad23051fd1d775cfd6
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-04-15 14:58:23 +0200

    fix(fzfmenu): include bash and handle options

--- packages/scripts/fzfmenu.sh
+++ packages/scripts/fzfmenu.sh
@@ -9,10 +9,32 @@ mkfifo "$input"
 mkfifo "$output"
 chmod 600 "$input" "$output"
 
+for i in "$@"; do
+  case $i in
+    -p)
+      PROMPT="$2"
+      shift
+      shift
+      break ;;
+    -l)
+      # no reason to filter number of lines
+      shift
+      shift
+      break ;;
+    -i)
+      # we do this anyway
+      shift
+      break ;;
+    *)
+      echo "Unknown option $1" >&2
+      shift ;;
+  esac
+done
+
 # it's better to use st here (starts a lot faster than pretty much everything else)
 st -c fzfmenu -n fzfmenu -g 85x10 \
   -e dash \
-  -c "cat $input | fzf --print-query $* | tee $output" & disown
+  -c "cat $input | fzf --reverse --prompt='${PROMPT+> }' --print-query $* | tee $output" & disown
 
 # handle ctrl+c outside child terminal window
 trap 'kill $! 2>/dev/null; rm -f $input $output' EXIT
