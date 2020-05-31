99f35d226e405a05a2a8fc0f6fc0453f2c3e3bb8
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-05-31 18:23:08 +0200

    feat(alacritty): add flipped variant

--- configs/alacritty.nix
+++ configs/alacritty.nix
@@ -1,17 +1,8 @@
 { pkgs, lib, config, ... }:
-{
-  environment.variables.TERMINAL = "alacritty";
-
-  environment.systemPackages = with pkgs; [
-    alacritty
-    alacritty.terminfo
-  ];
-
-  home-manager.users.me.xdg.configFile."alacritty/alacritty.yml".text =
-  let
-    colourNames = [ "black" "red" "green" "yellow" "blue" "magenta" "cyan" "white" ];
-    colours = lib.getAttrs colourNames config.niveum.colours;
-  in builtins.toJSON {
+let
+  colourNames = [ "black" "red" "green" "yellow" "blue" "magenta" "cyan" "white" ];
+  colours = lib.getAttrs colourNames config.niveum.colours;
+  alacrittyConfig = {
     background_opacity = 0.9;
     colors = {
       primary = {
@@ -27,6 +18,28 @@
     key_bindings = [
       { key = "Add"; mods = "Control"; action = "IncreaseFontSize"; }
       { key = "Minus"; mods = "Control"; action = "DecreaseFontSize"; }
+      { key = "Key0"; mods = "Control"; action = "ResetFontSize"; }
     ];
+  };
+  flippedAlacrittyConfig = alacrittyConfig // {
+    colors.primary = {
+      background = config.niveum.colours.foreground;
+      foreground = config.niveum.colours.background;
+    };
+  };
+in
+{
+  environment.variables.TERMINAL = "alacritty";
+
+  environment.systemPackages = with pkgs; [
+    alacritty
+    alacritty.terminfo
+    (pkgs.writers.writeDashBin "alacritty-flipped" ''
+      ${pkgs.alacritty}/bin/alacritty --config-file ${pkgs.writeText "alacritty.yml" (builtins.toJSON flippedAlacrittyConfig)} $@
+    '')
+  ];
+
+  home-manager.users.me.xdg.configFile = {
+    "alacritty/alacritty.yml".text = builtins.toJSON alacrittyConfig;
   };
 }
