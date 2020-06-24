db2bd93cf86d7cd3880d303d472b52e89525eabd
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-06-24 11:52:02 +0200

    wilde: switch to SSD, add LUKS

--- systems/wilde/hardware-configuration.nix
+++ systems/wilde/hardware-configuration.nix
@@ -1,23 +1,24 @@
 { config, lib, pkgs, ... }: {
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
-  boot.initrd.availableKernelModules =
-    [ "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/b700a1f4-127e-40d3-a1cf-d194785e915b";
+    device = "/dev/disk/by-uuid/66ced7b9-cfa7-40dd-a488-18ef91a337e0";
     fsType = "ext4";
   };
 
+  boot.initrd.luks.devices."luksmap".device = "/dev/disk/by-uuid/03b6abd0-e9ce-49c8-9659-a1d94f645d0f";
+
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/4474-04C0";
+    device = "/dev/disk/by-uuid/E1B1-1B9C";
     fsType = "vfat";
   };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/d1d62358-dce2-4081-849c-b4a9852c1c1b"; }];
+  swapDevices = [];
 
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
