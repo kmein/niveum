c4574909deead41b465414d0973698be63d7e7a3
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-07-11 20:45:57 +0200

    fix(mpv): get youtube in 720p

--- configs/mpv.nix
+++ configs/mpv.nix
@@ -1,21 +1,25 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    mpv
-    mpv-poll
-  ];
+{ pkgs, ... }: let
+  swallow = command: "${pkgs.scripts.swallow}/bin/swallow ${command}";
+in {
+  environment.shellAliases.mpv = swallow "mpv";
 
-  environment.shellAliases.mpv = "${pkgs.utillinux}/bin/setsid -f ${pkgs.mpv}/bin/mpv";
-
-  home-manager.users.me.xdg.configFile = {
-    "mpv/input.conf".text = ''
-      Alt+RIGHT add video-rotate 90
-      Alt+LEFT add video-rotate -90
-      Alt+- add video-zoom -0.25
-      Alt+= add video-zoom 0.25
-      Alt+l add video-pan-x -0.05
-      Alt+h add video-pan-x 0.05
-      Alt+k add video-pan-y 0.05
-      Alt+j add video-pan-y -0.05
-    '';
+  home-manager.users.me = {
+    programs.mpv = {
+      enable = true;
+      config = {
+        force-window = "yes";
+        ytdl-format = "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best";
+      };
+      bindings = {
+        "Alt+RIGHT" = "add video-rotate 90";
+        "Alt+LEFT" = "add video-rotate -90";
+        "Alt+-" = "add video-zoom -0.25";
+        "Alt+=" = "add video-zoom 0.25";
+        "Alt+l" = "add video-pan-x -0.05";
+        "Alt+h" = "add video-pan-x 0.05";
+        "Alt+k" = "add video-pan-y 0.05";
+        "Alt+j" = "add video-pan-y -0.05";
+      };
+    };
   };
 }
