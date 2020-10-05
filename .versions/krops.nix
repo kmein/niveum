717355a2cd4372bd83b001f7be76888aed9ef1ee
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-10-05 21:27:20 +0200

    chore: update

--- .versions/krops.nix
+++
@@ -1,7 +0,0 @@
-{
-  "url": "https://cgit.krebsco.de/krops",
-  "rev": "55aa2c77ce8183f3d2b24f54efa33ab6a42e1e02",
-  "date": "2020-04-19T01:06:20+02:00",
-  "sha256": "00rvx9h596ync8wgic6xc9srzajzm28wlp9q7mv4z0jsb27l5bj8",
-  "fetchSubmodules": false
-}
