2073c517ca5c38b0d14bc41bb719bb9f493c0b51
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-10-07 08:57:08 +0200

    feat(wifi): add krebs

--- configs/default.nix
+++ configs/default.nix
@@ -181,29 +181,24 @@ in {
         userControlled.enable = true;
         networks = {
           "Aether" = {
-            pskRaw =
-              "e1b18af54036c5c9a747fe681c6a694636d60a5f8450f7dec0d76bc93e2ec85a";
+            pskRaw = "e1b18af54036c5c9a747fe681c6a694636d60a5f8450f7dec0d76bc93e2ec85a";
             priority = 10;
           };
           "Asoziales Netzwerk" = {
-            pskRaw =
-              "8e234041ec5f0cd1b6a14e9adeee9840ed51b2f18856a52137485523e46b0cb6";
+            pskRaw = "8e234041ec5f0cd1b6a14e9adeee9840ed51b2f18856a52137485523e46b0cb6";
             priority = 10;
           };
           "Libertarian WiFi" = {
-            pskRaw =
-              "e9beaae6ffa55d10e80b8a2e7d997411d676a3cc6f1f29d0b080391f04555050";
+            pskRaw = "e9beaae6ffa55d10e80b8a2e7d997411d676a3cc6f1f29d0b080391f04555050";
             priority = 9;
           };
-          "EasyBox-927376".pskRaw =
-            "dbd490ab69b39bd67cfa06daf70fc3ef3ee90f482972a668ed758f90f5577c22";
+          "EasyBox-927376".pskRaw = "dbd490ab69b39bd67cfa06daf70fc3ef3ee90f482972a668ed758f90f5577c22";
           "FlixBus Wi-Fi" = { };
           "FlixBus" = { };
           "FlixTrain" = { };
           "BVG Wi-Fi" = { };
           "Hotel_Krone" = { }; # login: http://192.168.10.1/
-          "Ni/Schukajlow".pskRaw =
-            "ffc47f6829da59c48aea878a32252223303f5c47a3859edc90971ffc63346781";
+          "Ni/Schukajlow".pskRaw = "ffc47f6829da59c48aea878a32252223303f5c47a3859edc90971ffc63346781";
           "WIFIonICE" = { }; # login: http://10.101.64.10/
           "WLAN-914742".psk = "67647139648174545446";
           "KDG-CEAA4".psk = "PBkBSmejcvM4";
@@ -217,6 +212,7 @@ in {
           "Mayflower".psk = "Fr31EsLan";
           "Born11".psk = "56LMVLbw840EGNWk0RYRqvgicx3FSO";
           "FactoryCommunityGuest".psk = "Factory4ever";
+          "krebs".psk = "aidsballs";
         };
       };
 
