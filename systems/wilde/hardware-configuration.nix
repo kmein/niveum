a0830070f313d06b8715dd636e8f3bf0f6077460
Kierán Meinhardt <kmein@posteo.de>
2020-11-02 15:42:52 +0100

    feat(wilde): add swapfile

--- systems/wilde/hardware-configuration.nix
+++ systems/wilde/hardware-configuration.nix
@@ -6,19 +6,25 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  boot.initrd.luks.devices."luksmap".device = "/dev/disk/by-uuid/03b6abd0-e9ce-49c8-9659-a1d94f645d0f";
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/66ced7b9-cfa7-40dd-a488-18ef91a337e0";
     fsType = "ext4";
   };
-
-  boot.initrd.luks.devices."luksmap".device = "/dev/disk/by-uuid/03b6abd0-e9ce-49c8-9659-a1d94f645d0f";
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/E1B1-1B9C";
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      priority = 0;
+      size = 4096;
+    }
+  ];
 
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
