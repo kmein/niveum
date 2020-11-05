80c3b143ca24d96b3a70b864151a9d7e8ac33d30
Kierán Meinhardt <kmein@posteo.de>
2020-11-05 21:29:20 +0100

    feat: write own urlwatch config

--- configs/urlwatch.nix
+++ configs/urlwatch.nix
@@ -1,16 +1,79 @@
-{ lib, ... }:
-{
-  imports = [ <stockholm/krebs/3modules/urlwatch.nix> ];
+{ pkgs, config, lib, ... }:
+let
+  inherit (import <niveum/lib>) kieran;
 
-  krebs.urlwatch = {
-    enable = true;
-    onCalendar = "*-*-* 05:00:00";
-    sendmail.enable = false;
-    telegram = {
-      enable = true;
-      chatId = [ "18980945" ];
-      botToken = lib.strings.fileContents <system-secrets/telegram/kmein.token>;
+  urlwatchDir = "/var/lib/urlwatch";
+
+  urlsFile = pkgs.writeText "urls" (builtins.concatStringsSep "\n---\n" (map builtins.toJSON urls));
+
+  urls = [
+    {
+      name = "Corona-Verordnung";
+      url = "https://www.berlin.de/corona/massnahmen/verordnung/";
+    }
+  ];
+
+  configFile = pkgs.writeJSON "urlwatch.yaml" {
+    display = {
+      error = true;
+      new = true;
+      unchanged = false;
     };
-    urls = [ ];
+    report = {
+      email = {
+        enabled = true;
+        from = "2210@cock.li";
+        html = false;
+        method = "smtp";
+        smtp = {
+          host = "mail.cock.li";
+          port = 587;
+          starttls = true;
+          auth = true;
+          insecure_password = lib.strings.fileContents <secrets/mail/cock>;
+        };
+        subject = "{count} changes: {jobs}";
+        to = kieran.email;
+      };
+      html.diff = "unified";
+      stdout = {
+        color = true;
+        enabled = true;
+      };
+      telegram = {
+        enabled = false;
+        bot_token = lib.strings.fileContents <system-secrets/telegram/kmein.token>;
+        chat_id = [ "18980945" ];
+      };
+      # text = {
+      #   details = true;
+      #   footer = true;
+      #   line_length = 75;
+      # };
+    };
+  };
+in
+{
+  users.extraUsers.urlwatch = {
+    home = urlwatchDir;
+    createHome = true;
+  };
+
+  systemd.services.urlwatch = {
+    enable = true;
+    startAt = "*-*-* 05:00:00";
+    script = ''
+      ${pkgs.urlwatch}/bin/urlwatch \
+        --config=${lib.escapeShellArg configFile} \
+        --urls=${lib.escapeShellArg urlsFile}
+    '';
+    serviceConfig = {
+      User = config.users.extraUsers.urlwatch.name;
+      WorkingDirectory = config.users.extraUsers.urlwatch.home;
+      PermissionsStartOnly = "true";
+      PrivateTmp = "true";
+      SyslogIdentifier = "urlwatch";
+      Type = "oneshot";
+    };
   };
 }
