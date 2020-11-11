46882af11ee7fc82bc745f02cc168d41a347ab70
Kierán Meinhardt <kmein@posteo.de>
2020-11-11 08:51:49 +0100

    feat: run gitea on makanek

--- systems/makanek/configuration.nix
+++ systems/makanek/configuration.nix
@@ -12,6 +12,20 @@ in
     <niveum/configs/save-space.nix>
     <niveum/configs/version.nix>
     <niveum/modules/retiolum.nix>
+    {
+      services.gitea = {
+        enable = true;
+        disableRegistration = true;
+        rootUrl = "https://code.xn--kiern-0qa.de";
+        appName = "code.kierán.de";
+      };
+      networking.firewall.allowedTCPPorts = [ config.services.gitea.httpPort ];
+      services.nginx.virtualHosts."code.xn--kiern-0qa.de"  ={
+        forceSSL = true;
+        enableACME = true;
+        locations."/".extraConfig = "proxy_pass http://localhost:3000;";
+      };
+    }
   ];
 
   boot.loader.grub.enable = true;
