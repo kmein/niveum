cea93cc9410f509a289c5bea53c64f10ec9eaa08
Kierán Meinhardt <kmein@posteo.de>
2020-12-15 16:09:18 +0100

    feat(wallpaper): use lassulus worldmap

--- configs/wallpaper.nix
+++ configs/wallpaper.nix
@@ -16,14 +16,28 @@ let
        rev = "33cad3099919366cea2627f930da9b47609e8554";
        sha256 = "1li6rrn016fpgvmnijqhvkp07kj83cjwcjx2l2b3asb99d51814i";
      };
-  */
   kmein-wallpapers = pkgs.fetchFromGitHub {
     owner = "kmein";
     repo = "wallpapers";
     rev = "7c553bc6bd78afa6dbf2824691466bbad0d8e6e9";
     sha256 = "1zik5z1cq1084j1hdwm204afz89f5hpg21z0vvcbppzkmldfxnnq";
   };
+  */
 in {
+  imports = [ <stockholm/krebs/3modules/fetchWallpaper.nix> ];
+
+  krebs.fetchWallpaper = {
+    enable = true;
+    unitConfig.ConditionPathExists = "!/var/run/ppp0.pid";
+    url = "http://prism.r/realwallpaper.png";
+  };
+
+  services.xserver = {
+    display = lib.mkForce 0; # needed for fetchWallpaper to find the X display
+    displayManager.sessionCommands = "${pkgs.xorg.xhost}/bin/xhost +LOCAL:"; 
+  };
+
+  /*
   home-manager.users.me = {
     services.random-background = {
       enable = true;
@@ -31,4 +45,5 @@ in {
       interval = "1h";
     };
   };
+  */
 }
