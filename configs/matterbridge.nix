3c837ab3ee7a6a86c88bd85742b0394c40147274
Kierán Meinhardt <kmein@posteo.de>
2021-01-01 02:46:07 +0100

    Revert "feat(matterbridge): add nixos-rc3 bridge"
    This reverts commit 9c1bf37d50c481ffbbe252da7ee1cf2ed3620628.

--- configs/matterbridge.nix
+++ configs/matterbridge.nix
@@ -16,11 +16,6 @@ let
       Nick = "krebs_bridge";
       SkipTLSVerify = true;
     };
-    mumble.nixos-rc3 = {
-      Server = "lassul.us:64738";
-      Nick = "irc_bridge";
-      SkipTLSVerify = true;
-    };
     gateway = [
       {
         name = "krebs-bridge";
@@ -37,20 +32,6 @@ let
           {
             account = "mumble.lassulus";
             channel = 6; # "nixos"
-          }
-        ];
-      }
-      {
-        name = "nixos-rc3";
-        enable = true;
-        inout = [
-          {
-            account = "irc.freenode";
-            channel = "#nixos-rc3";
-          }
-          {
-            account = "mumble.nixos-rc3";
-            channel = 11; # "nixos/rc3"
           }
         ];
       }
