3b0a5afad0dcdb0e4d75cacc30e6f6f0f8683d61
Kierán Meinhardt <kmein@posteo.de>
2021-01-23 17:12:08 +0100

    feat(zaatar): add worldradio

---
+++ packages/worldradio.nix
@@ -0,0 +1,14 @@
+{ jq, runCommand, fetchFromGitHub }:
+let
+  online-radio = fetchFromGitHub {
+    owner = "josiahmokob0";
+    repo = "online-radio";
+    rev = "04d16421355196194a05e5504ba8f66a9c07ee7b";
+    sha256 = "1q0iagx7df0sd6vl5anvpzyiw4jdwa6c67z45rx622a6cr6m4zzl";
+  };
+in runCommand "worldradio.m3u" {} ''
+  ${jq}/bin/jq --raw-output --slurp 'flatten | map(.url_resolved) | .[]' ${online-radio}/src/data/countries/*.json \
+    | sort \
+    | uniq \
+    > $out
+''
