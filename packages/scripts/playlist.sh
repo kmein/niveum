6843ac90b5c11c59fe2560f342f38e280c1ad78d
Kierán Meinhardt <kmein@posteo.de>
2021-01-27 09:54:16 +0100

    feat(pls): notify in irc channel

--- packages/scripts/playlist.sh
+++ packages/scripts/playlist.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
+send_irc() {
+  echo "
+    USER $USER $(hostname) tolmoon $USER
+    NICK musikkritiker
+    JOIN #the_playlist
+    PRIVMSG #the_playlist $*
+    QUIT
+  " | nc irc.freenode.net 6667 >/dev/null
+}
+
+
+
 endpoint=prism.r:8001
 
 case "$1" in
   good|like|cool|nice|yes|+)
-    curl -sS -XPOST "$endpoint/good";;
+    send_irc 'nice!' &
+    curl -sS -XPOST "$endpoint/good"
+  ;;
   skip|next|bad|sucks|no|nope|-)
-    curl -sS -XPOST "$endpoint/skip";;
+    send_irc 'sucks' &
+    curl -sS -XPOST "$endpoint/skip"
+  ;;
   *)
-    curl -sS -XGET "$endpoint/current" | jq;;
+    curl -sS -XGET "$endpoint/current" | jq
+  ;;
 esac
+wait
