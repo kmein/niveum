8b534e3aee78f1a97e6a3a755b162052b3a6d13b
Kierán Meinhardt <kmein@posteo.de>
2021-02-07 22:03:24 +0100

    feat(weechat): install colorize_nicks and autosort scripts

--- configs/weechat.nix
+++ configs/weechat.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
-let inherit (import <niveum/lib>) kieran;
+let
+  inherit (import <niveum/lib>) kieran nixpkgs-unstable;
 in {
   systemd.services.weechat =
   let
@@ -21,21 +22,30 @@ in {
     '';
     weechat = pkgs.weechat.override {
       configure = { ... }: {
+        scripts = [ pkgs.weechatScripts.weechat-autosort nixpkgs-unstable.weechatScripts.colorize_nicks ];
         init = ''
           /set irc.server_default.nicks "kmein,kfm,kieran"
           /set irc.server_default.msg_part "tschö mit ö"
           /set irc.server_default.msg_quit "ciao kakao"
           /set irc.server_default.msg_kick "warum machst du diese?"
           /set irc.server_default.realname "${kieran.name}"
+
+          /server add hackint irc.hackint.org/6697 -ipv6 -ssl -autoconnect
           /server add freenode chat.freenode.org
           /server add irc.r irc.r
           /server add news.r news.r
+
           /alias add mod /quote omode $channel +o $nick
+
           /set irc.server.freenode.autojoin "#krebs,#flipdot,##myengadin"
           /set irc.server.irc.r.autojoin "#xxx,#brockman"
           /set irc.server.news.r.autojoin "#drachengame,#memes,#all,#berlin"
           /set logger.level.irc.news.r.#all 0
-          /filter addreplace corona irc.news.r.* * corona|covid|virus|lockdown|va[kc][sc]in|mutante|mutation|impf|pandemi
+
+          /filter addreplace corona irc.news.r.* * [kc]orona|[kc]ovid|virus|lockdown|va[kc][sc]in|mutante|mutation|impf|pandemi
+
+          /set irc.look.server_buffer independent
+
           /connect freenode
           /connect irc.r
           /connect news.r
@@ -46,7 +56,7 @@ in {
     description = "Weechat bouncer";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
-    restartIfChanged = false;
+    restartIfChanged = true;
     path = [ pkgs.alacritty.terminfo ];
     environment.WEECHAT_HOME = "/var/lib/weechat";
     script = "${tmux} -2 new-session -d -s IM ${weechat}/bin/weechat";
