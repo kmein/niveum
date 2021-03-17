759c82f5b53374660237f54c795598d99ccabbcc
Kierán Meinhardt <kmein@posteo.de>
2021-03-17 09:55:46 +0100

    feat(chromium): goodbye lastpass, hello bitwarden

--- configs/chromium.nix
+++ configs/chromium.nix
@@ -2,7 +2,7 @@
   programs.chromium = {
     enable = true;
     extensions = [
-      "hdokiejnpimakedhajhdlcegeplioahd" # LastPass
+      "nngceckbapebfimnlniiiahkandclblb" # BitWarden
       # "ihlenndgcmojhcghmfjfneahoeklbjjh" # cVim
       # "fpnmgdkabkmnadcjpehmlllkndpkmiak" # Wayback Machine
       "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
