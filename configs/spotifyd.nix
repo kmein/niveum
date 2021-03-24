48ce381be0ca3f1c939400d335dfa74fb62f282b
Kierán Meinhardt <kmein@posteo.de>
2021-03-24 20:26:05 +0100

    fix(spotifyd): run correctly with systemWide pulseaudio

--- configs/spotifyd.nix
+++ configs/spotifyd.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   services.spotifyd = {
     enable = true;
@@ -6,9 +6,19 @@
       global = {
         username = lib.strings.fileContents <secrets/spotify/username>;
         password = lib.strings.fileContents <secrets/spotify/password>;
+        backend = "pulseaudio";
+        bitrate = 320;
+        device_type = "s_t_b"; # set-top box
+        device_name = config.networking.hostName;
       };
     };
   };
+
+  # ref https://github.com/NixOS/nixpkgs/issues/71362#issuecomment-753461502
+  hardware.pulseaudio.extraConfig = ''
+    unload-module module-native-protocol-unix
+    load-module module-native-protocol-unix auth-anonymous=1
+  '';
 
   systemd.services.spotifyd.serviceConfig.Restart = "always";
 }
