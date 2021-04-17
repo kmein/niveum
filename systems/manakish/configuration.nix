07462e91528eabcd542e8d30a71ff929bb03c421
Kierán Meinhardt <kmein@posteo.de>
2021-04-17 09:27:49 +0200

    feat(manakish): prompt color green, automatically set xrandr and pulseaudio profiles

--- systems/manakish/configuration.nix
+++ systems/manakish/configuration.nix
@@ -12,13 +12,24 @@
       programs.steam.enable = true;
       nixpkgs.config.steam.java = true;
     }
+    {
+      services.xserver.displayManager.sessionCommands =
+      let
+        intern = "LVDS-1";
+        extern = "HDMI-1";
+        pulseaudioCard = "alsa_card.pci-0000_00_1b.0";
+        pulseaudioProfile = "output:hdmi-stereo+input:analog-stereo";
+      in toString (pkgs.writers.writeDash "hdmi-on" ''
+        ${pkgs.xorg.xrandr}/bin/xrandr --output ${intern} --primary --auto --output ${extern} --above ${intern} --auto
+        ${pkgs.pulseaudio}/bin/pactl set-card-profile ${pulseaudioCard} ${pulseaudioProfile}
+      '');
+    }
   ];
-
 
   niveum = {
     batteryName = "BAT0";
     wirelessInterface = "wlp3s0";
-    promptColours.success = "cyan";
+    promptColours.success = "green";
   };
 
   networking.useDHCP = false;
