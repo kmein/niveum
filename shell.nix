f2bda30984ae3fd026fd2c984afcc20303b3a9d9
Kierán Meinhardt <kmein@posteo.de>
2021-06-05 18:13:08 +0200

    fix: take stockholm from krebsco.de

--- shell.nix
+++ shell.nix
@@ -27,7 +27,7 @@ let
     };
     stockholm = {
       ref = "refs/heads/master";
-      url = "https://github.com/kmein/stockholm";
+      url = "https://cgit.krebsco.de/stockholm";
       path = toString .versions/stockholm.json;
     };
     retiolum = {
