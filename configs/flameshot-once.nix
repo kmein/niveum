1131da55ce42ebd5ec41fe135687e908cef33957
Kierán Meinhardt <kmein@posteo.de>
2021-06-07 15:34:26 +0200

    fix(flameshot): configurate with nix

--- configs/flameshot-once.nix
+++ configs/flameshot-once.nix
@@ -3,19 +3,23 @@
   home-manager.users.me = {
     services.flameshot.enable = true;
 
-    home.file.".config/Dharkael/flameshot.ini".source = (pkgs.formats.ini {}).generate "flameshot.ini" {
+    xdg.configFile."flameshot/flameshot.ini".source = (pkgs.formats.ini {}).generate "flameshot.ini" {
       General = {
         disabledTrayIcon = true;
-        drawColor = ''@Variant(\0\0\0\x43\x1\xff\xff\0\0\0\0\xff\xff\0\0)'';
+        checkForUpdates = false;
+        contrastOpacity = 188;
+        savePath = "/tmp";
+        savePathFixed = true;
         drawThickness = 0;
+        showStartupLaunchMessage = false;
         filenamePattern = "shot_%F_%T";
       };
     };
 
     systemd.user.services.flameshot.Unit.Requires = lib.mkForce [];
     systemd.user.services.flameshot.Environment = {
-      IMGUR_CREATE_URL = "https://p.krebsco.de/image";
-      IMGUR_DELETE_URL = "https://p.krebsco.de/image/delete/%1";
+      # IMGUR_CREATE_URL = "https://p.krebsco.de/image";
+      # IMGUR_DELETE_URL = "https://p.krebsco.de/image/delete/%1";
       PATH = "${config.home-manager.users.me.home.profileDirectory}/bin";
     };
   };
