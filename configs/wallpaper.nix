eca981e5c9dadc88474c008945ec43786cab2b85
Kierán Meinhardt <kmein@posteo.de>
2021-06-10 20:13:32 +0200

    Revert "fix(fetchWallpaper)"
    This reverts commit d23d443cc56ad855d4814fe43b7f9d359eb8fe55.

--- configs/wallpaper.nix
+++ configs/wallpaper.nix
@@ -7,6 +7,8 @@
     url = "http://prism.r/realwallpaper-krebs-stars-berlin.png";
   };
 
+  users.users.fetchWallpaper.isSystemUser = true;
+
   services.xserver = {
     display = lib.mkForce 0; # needed for fetchWallpaper to find the X display
     displayManager.sessionCommands = "${pkgs.xorg.xhost}/bin/xhost +LOCAL:";
