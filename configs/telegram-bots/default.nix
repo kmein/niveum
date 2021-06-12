af6c9954261d89d2dab05d08b20d4bbf99eb044d
Kierán Meinhardt <kmein@posteo.de>
2021-06-12 16:57:42 +0200

    feat(telegram bots): get proverb bot from telebots

--- configs/telegram-bots/default.nix
+++ configs/telegram-bots/default.nix
@@ -4,19 +4,28 @@ let
     telebots-package = pkgs.fetchFromGitHub {
       owner = "kmein";
       repo = "telebots";
-      rev = "ce613f5b298721c6eaa7af25a666bd65388a0daf";
-      sha256 = "0bl02976kxjbbbmvcnjjgpmxrmpwplw9qcldxk5h3wngzixxizhg";
+      rev = "22931c9457e092c4e413555dbe61819d77844246";
+      sha256 = "0byp3w6li3fin7ry0ki4rmgkaajdil424y5pc1j7ci9mpws0s5ik";
     };
   in pkgs.callPackage telebots-package {};
+  reverseDirectory = "/run/telegram-reverse";
+  proverbDirectory = "/run/telegram-proverb";
+  inherit (import <niveum/lib>) tmpfilesConfig;
 in {
   imports = [
     ./literature-quote.nix
     ./autorenkalender.nix
-    ./proverb.nix
     ./nachtischsatan.nix
     ./tlg-wotd.nix
     <niveum/modules/telegram-bot.nix>
   ];
+
+  systemd.tmpfiles.rules = map (path: tmpfilesConfig {
+    type = "d";
+    mode = "0750";
+    age = "1h";
+    inherit path;
+  }) [ reverseDirectory proverbDirectory ];
 
   systemd.services.telegram-reverse = {
     wantedBy = [ "multi-user.target" ];
@@ -26,6 +35,7 @@ in {
     enable = true;
     script = "${telebots}/bin/telegram-reverse";
     serviceConfig.Restart = "always";
+    serviceConfig.WorkingDirectory = reverseDirectory;
   };
 
   systemd.services.telegram-betacode = {
@@ -35,5 +45,15 @@ in {
     enable = true;
     script = "${telebots}/bin/telegram-betacode";
     serviceConfig.Restart = "always";
+  };
+
+  systemd.services.telegram-proverb = {
+    wantedBy = [ "multi-user.target" ];
+    description = "Telegram proverb bot";
+    environment.TELEGRAM_BOT_TOKEN = lib.strings.fileContents <system-secrets/telegram/proverb.token>;
+    enable = true;
+    script = "${telebots}/bin/telegram-proverb";
+    serviceConfig.Restart = "always";
+    serviceConfig.WorkingDirectory = proverbDirectory;
   };
 }
