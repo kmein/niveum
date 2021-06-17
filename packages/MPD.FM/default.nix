1311b6482484e8392def72d908f41c99ccb4e00c
Kierán Meinhardt <kmein@posteo.de>
2021-06-17 07:37:19 +0200

    feat(mpd-fm): update

--- packages/MPD.FM/default.nix
+++ packages/MPD.FM/default.nix
@@ -4,8 +4,8 @@ mkYarnPackage rec {
   src = fetchFromGitHub {
     owner = "kmein";
     repo = "MPD.FM";
-    rev = "5f309c2579a9cbbbc4f7eb6a2e2f3993cc177630";
-    sha256 = "0ladh96s656i7yd9qxrpqq4x513r88zas7112rqn5sgxxaccbh72";
+    rev = "9dd05f5914ece489411e4f1225444aeb18c8e9df";
+    sha256 = "06m360qsx88m83bl64mpxngkqmhday80wf15y9s5w7r8lr4zyhpa";
   };
   packageJSON = "${src}/package.json";
   yarnLock = ./yarn.lock;
