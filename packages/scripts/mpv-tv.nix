d025d2a289b6ac88048ee57de93921e67ce47883
Kierán Meinhardt <kmein@posteo.de>
2021-07-19 13:41:50 +0200

    feat(mpv-tv): add postFetch

--- packages/scripts/mpv-tv.nix
+++ packages/scripts/mpv-tv.nix
@@ -1,12 +1,13 @@
 { pkgs, lib }:
 let
-  streams-m3u = pkgs.fetchurl {
+  streams-tsv = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/jnk22/kodinerds-iptv/master/iptv/kodi/kodi_tv.m3u";
-    sha256 = "1fd91y87cydhxn6brisn4hkx73z6axhgr1fjamayg0scffyapir8";
+    sha256 = "1cw1gmb16vwh6qfw0z3wjjfgn0zg2qplnddqzva5b0xx2g2appla";
+    postFetch = ''
+      ${pkgs.gnused}/bin/sed '/#EXTM3U/d;/#EXTINF/s/.*,//g' $out | ${pkgs.coreutils}/bin/paste -d'\t' - - > $out.tmp
+      mv $out.tmp $out
+    '';
   };
-  streams-tsv = pkgs.runCommand "streams.tsv" {} ''
-    ${pkgs.gnused}/bin/sed '/#EXTM3U/d;/#EXTINF/{s/\r\n//g; s/.*,//g}' ${streams-m3u} | ${pkgs.coreutils}/bin/paste -d'\t' - - > $out
-  '';
 in pkgs.writers.writeDashBin "mpv-tv" ''
   exec ${pkgs.mpv}/bin/mpv --force-window=yes "$(${pkgs.dmenu}/bin/dmenu -i -l 5 < ${streams-tsv} | ${pkgs.coreutils}/bin/cut -f2)"
 ''
