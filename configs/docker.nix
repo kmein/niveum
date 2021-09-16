136b04cb3f3e900b16018a83cb8a955153334bf6
Kierán Meinhardt <kmein@posteo.de>
2021-09-16 15:23:02 +0200

    fix(docker): go around ICE wifi

--- configs/docker.nix
+++ configs/docker.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }: {
-  virtualisation.docker.enable = true;
+{ lib, pkgs, ... }: {
+  virtualisation.docker = {
+    enable = true;
+    # for ICE wifi, ref https://gist.github.com/sunsided/7840e89ff4e11b64a2d7503fafa0290c
+    extraOptions = lib.concatStringsSep " " [
+      "--bip=172.39.1.5/24"
+      "--fixed-cidr=172.39.1.0/25"
+    ];
+  };
   users.users.me.extraGroups = [ "docker" ];
   environment.systemPackages = [ pkgs.docker pkgs.docker_compose ];
 }
