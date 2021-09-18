8b8fbeb511f012f5671551397f023ab25c1a18e4
Kierán Meinhardt <kmein@posteo.de>
2021-09-18 14:24:32 +0200

    feat(moodle-dl): run on zaatar for meinhark

--- systems/toum/configuration.nix
+++ systems/toum/configuration.nix
@@ -8,7 +8,6 @@ in {
     <niveum/configs/distrobump.nix>
     <niveum/configs/spacetime.nix>
     <niveum/configs/sshd.nix>
-    <niveum/configs/moodle-dl/meinhark.nix>
     <niveum/configs/save-space.nix>
     <niveum/configs/tmux.nix>
     <niveum/configs/version.nix>
