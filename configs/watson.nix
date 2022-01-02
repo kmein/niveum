e9d3a162ca7735f173c621bed3eebbb98a623a51
Kierán Meinhardt <kmein@posteo.de>
2022-01-02 12:16:45 +0100

    feat(watson): symlink directory from cloud

--- configs/watson.nix
+++ configs/watson.nix
@@ -1,4 +1,8 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.watson ];
+
+  system.activationScripts.watson-home = ''
+    ln -sfn ${config.users.users.me.home}/cloud/Seafile/Documents/watson ${config.users.users.me.home}/.config/watson
+  '';
 }
