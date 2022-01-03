8efa7f08ba1310d9386536b6423dd042000fae1d
Kierán Meinhardt <kmein@posteo.de>
2022-01-03 11:49:14 +0100

    feat(watson): manage config

--- configs/watson.nix
+++ configs/watson.nix
@@ -2,7 +2,17 @@
 {
   environment.systemPackages = [ pkgs.watson ];
 
+  home-manager.users.me.xdg.configFile."watson/config".text = ''
+    [options]
+    confirm_new_project = true
+    confirm_new_tag = true
+    date_format = %Y-%m-%d
+    log_current = true
+    report_current = true
+  '';
+
   system.activationScripts.watson-home = ''
-    ln -sfn ${config.users.users.me.home}/cloud/Seafile/Documents/watson ${config.users.users.me.home}/.config/watson
+    ln -sfn ${config.users.users.me.home}/cloud/Seafile/Documents/watson/frames ${config.users.users.me.home}/.config/watson/frames
+    ln -sfn ${config.users.users.me.home}/cloud/Seafile/Documents/watson/state ${config.users.users.me.home}/.config/watson/state
   '';
 }
