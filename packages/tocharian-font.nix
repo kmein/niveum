462071eff019e3ce3c3fc0947324205342498752
Kierán Meinhardt <kmein@posteo.de>
2022-01-11 19:56:31 +0100

    feat: tocharian font

---
+++ packages/tocharian-font.nix
@@ -0,0 +1,24 @@
+{ stdenv, lib, fetchurl, mupdf }:
+stdenv.mkDerivation {
+  pname = "tocharian-font";
+  version = "unstable-2022-01-11";
+  src = fetchurl {
+    url = "https://unicode.org/L2/L2015/15236-tocharian.pdf";
+    sha256 = "08bzkva9a6b2cfl38p9m22b1cf6yv27xsw6nrvq5ly5nffjm32hv";
+  };
+  dontUnpack = true;
+  buildInputs = [ mupdf ];
+  buildPhase = ''
+    mutool extract $src
+  '';
+  installPhase = ''
+    mkdir -p $out/share/fonts/truetype
+    install font-0021.ttf $out/share/fonts/truetype/Tocharian.ttf
+  '';
+  meta = with lib; {
+    description = "Tocharian font by Lee Wilson";
+    license = licenses.unfree;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ kmein ];
+  };
+}
