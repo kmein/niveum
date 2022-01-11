25ce9bcad829711b322b181f3cd9005647126a69
Kierán Meinhardt <kmein@posteo.de>
2022-01-11 23:18:43 +0100

    feat: stable htmlq

--- configs/lb.nix
+++ configs/lb.nix
@@ -1,7 +1,4 @@
 { lib, pkgs, ... }:
-let
-  nixpkgs-unstable = import <nixpkgs-unstable> {};
-in
 {
   systemd.services.lb-subscription = {
     enable = true;
@@ -15,10 +12,10 @@ in
       first_year=2019
       for year in $(${pkgs.coreutils}/bin/seq "$first_year" "$(date +%Y)"); do
         ${pkgs.curl}/bin/curl -sSL "https://www.literarische-blaetter.de/jahrgang-$year/" \
-          | ${nixpkgs-unstable.htmlq}/bin/htmlq --attribute href 'ul.slides a' \
+          | ${pkgs.htmlq}/bin/htmlq --attribute href 'ul.slides a' \
           | while read -r month; do
             ${pkgs.curl}/bin/curl -sSL "$month" \
-              | ${nixpkgs-unstable.htmlq}/bin/htmlq --attribute src iframe \
+              | ${pkgs.htmlq}/bin/htmlq --attribute src iframe \
               | ${pkgs.gnused}/bin/sed 's/.*?pdf=//;s/?wp-hosted.*//'
           done
       done | ${pkgs.findutils}/bin/xargs ${pkgs.wget}/bin/wget --no-clobber
