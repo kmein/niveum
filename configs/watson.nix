0a2061fe5c861a21a92e554ca07ab5aa824b8a1f
Kierán Meinhardt <kmein@posteo.de>
2022-01-14 08:30:33 +0100

    feat(watson): configure WATSON_DIR

--- configs/watson.nix
+++ configs/watson.nix
@@ -2,17 +2,5 @@
 {
   environment.systemPackages = [ pkgs.watson ];
 
-  home-manager.users.me.xdg.configFile."watson/config".text = ''
-    [options]
-    confirm_new_project = true
-    confirm_new_tag = true
-    date_format = %Y-%m-%d
-    log_current = true
-    report_current = true
-  '';
-
-  system.activationScripts.watson-home = ''
-    ln -sfn ${config.users.users.me.home}/cloud/Seafile/Documents/watson/frames ${config.users.users.me.home}/.config/watson/frames
-    ln -sfn ${config.users.users.me.home}/cloud/Seafile/Documents/watson/state ${config.users.users.me.home}/.config/watson/state
-  '';
+  environment.variables.WATSON_DIR = "${config.users.users.me.home}/cloud/Seafile/Documents/watson";
 }
