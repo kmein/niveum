20da594f97d25f9df0b16819519a59ca603067e1
Kierán Meinhardt <kmein@posteo.de>
2022-01-18 23:36:38 +0100

    feat(restic): backup git

--- configs/backup.nix
+++ configs/backup.nix
@@ -8,8 +8,13 @@ in
     inherit (restic) repository;
     timerConfig = { OnCalendar = "00:05"; RandomizedDelaySec = "5h"; };
     passwordFile = toString <secrets/restic/password>;
+    extraBackupArgs = [
+      "--exclude-file=/home/kfm/projects/nixpkgs/.git"
+      "--exclude=node_modules"
+    ];
     paths = [
       "/home/kfm/work"
+      "/home/kfm/projects"
     ];
   };
 
