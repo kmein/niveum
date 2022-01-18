d179f7ede5f3e7146c890bb157bddfe699842cea
Kierán Meinhardt <kmein@posteo.de>
2022-01-19 00:48:15 +0100

    feat(restic): backup regular systems

--- configs/backup.nix
+++ configs/backup.nix
@@ -9,12 +9,15 @@ in
     timerConfig = { OnCalendar = "00:05"; RandomizedDelaySec = "5h"; };
     passwordFile = toString <secrets/restic/password>;
     extraBackupArgs = [
-      "--exclude-file=/home/kfm/projects/nixpkgs/.git"
+      "--exclude=/home/kfm/projects/nixpkgs/.git"
       "--exclude=node_modules"
     ];
     paths = [
       "/home/kfm/work"
       "/home/kfm/projects"
+      "/home/kfm/cloud"
+      "/home/kfm/.gnupg"
+      "/home/kfm/.ssh"
     ];
   };
 
