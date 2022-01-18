3638e93ba19c224a6a9a7a5dcc76eeb9e9a1815c
Kierán Meinhardt <kmein@posteo.de>
2022-01-18 22:38:28 +0100

    feat(restic): add wrapper command

--- configs/backup.nix
+++ configs/backup.nix
@@ -1,11 +1,21 @@
+{ pkgs, config, ... }:
+let
+  repository = "rest:http://zaatar.r:3571/";
+in
 {
   services.restic.backups.niveum = {
     initialize = true;
-    repository = "rest:http://zaatar.r:3571/";
+    inherit repository;
     timerConfig = { OnCalendar = "00:05"; RandomizedDelaySec = "5h"; };
     passwordFile = toString <secrets/restic/password>;
     paths = [
       "/home/kfm/work"
     ];
   };
+
+  environment.systemPackages = [
+    (pkgs.writers.writeDashBin "restic-niveum" ''
+      ${pkgs.restic}/bin/restic -r ${repository} -p ${<secrets/restic/password>} "$@"
+    '')
+  ];
 }
