638751eb20e3a0932984f4f04b01e6dd5212ff62
Kierán Meinhardt <kmein@posteo.de>
2022-01-23 16:40:54 +0100

    feat(rofi): theme

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -1,9 +1,10 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   inherit (import <niveum/lib>) colours;
 in{
   home-manager.users.me.programs.rofi = {
     enable = true;
     font = "Monospace 10";
+    theme = "${pkgs.rofi}/share/rofi/themes/Arc-Dark.rasi";
   };
 }
