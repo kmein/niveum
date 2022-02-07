ed04fd8d852592e96a59f31e1f41fd458e80d460
Kierán Meinhardt <kmein@posteo.de>
2022-02-07 11:31:48 +0100

    feat: #hsmr moinbot

---
+++ systems/makanek/moinbot.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  systemd.services.moinbot = {
+    startAt = "7:00";
+    script = ''
+      echo moin | ${pkgs.ircaids}/bin/ircsink \
+        --nick moinbot \
+        --server irc.hackint.org \
+        --port 6697 \
+        --secure \
+        --target '#hsmr' >/dev/null 2>&1
+    '';
+    serviceConfig.DynamicUser = true;
+  };
+
+  systemd.timers.moinbot.timerConfig.RandomizedDelaySec = "14h";
+}
