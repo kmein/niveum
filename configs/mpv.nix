d6332e884820c69582d9f8a00139e86674956ecf
Kierán Meinhardt <kmein@posteo.de>
2022-04-14 13:40:11 +0200

    feat(mpv): limit resolution

--- configs/mpv.nix
+++ configs/mpv.nix
@@ -18,6 +18,7 @@ in {
     programs.mpv = {
       enable = true;
       config = {
+        ytdl-format = "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best";
         ytdl-raw-options = lib.concatStringsSep "," [''sub-lang="de,en"'' "write-sub=" "write-auto-sub="];
         screenshot-template = "%F-%wH%wM%wS-%#04n";
       };
