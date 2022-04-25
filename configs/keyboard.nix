152410732782b725b4396f2d7f980f24c6253b41
Kierán Meinhardt <kmein@posteo.de>
2022-04-25 11:34:07 +0200

    feat(keyboard): add arabic and russian

--- configs/keyboard.nix
+++ configs/keyboard.nix
@@ -5,9 +5,10 @@
 }: let
   commaSep = builtins.concatStringsSep ",";
 in {
+  # man 7 xkeyboard-config
   services.xserver = {
-    layout = commaSep ["de" "gr"];
-    xkbVariant = commaSep ["T3" "polytonic"];
+    layout = commaSep ["de" "gr" "ru" "ara"];
+    xkbVariant = commaSep ["T3" "polytonic" "phonetic" "buckwalter"];
     xkbOptions =
       commaSep ["compose:caps" "terminate:ctrl_alt_bksp" "grp:ctrls_toggle"];
     libinput.enable = true;
