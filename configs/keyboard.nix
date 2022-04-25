04f4084c891a1d319c051d5f104ecccfebd251af
Kierán Meinhardt <kmein@posteo.de>
2022-04-25 11:52:27 +0200

    feat(keyboard): add docs

--- configs/keyboard.nix
+++ configs/keyboard.nix
@@ -8,6 +8,8 @@ in {
   # man 7 xkeyboard-config
   services.xserver = {
     layout = commaSep ["de" "gr" "ru" "ara"];
+    # T3: https://upload.wikimedia.org/wikipedia/commons/a/a9/German-Keyboard-Layout-T3-Version1-large.png
+    # buckwalter: http://www.qamus.org/transliteration.htm
     xkbVariant = commaSep ["T3" "polytonic" "phonetic" "buckwalter"];
     xkbOptions =
       commaSep ["compose:caps" "terminate:ctrl_alt_bksp" "grp:ctrls_toggle"];
