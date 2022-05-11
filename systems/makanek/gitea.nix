c1cef8bc33f83846caf6a0560f88c898e4e47465
Kierán Meinhardt <kmein@posteo.de>
2022-05-11 17:38:45 +0200

    fix(gitea): show correct SSH clone port

--- systems/makanek/gitea.nix
+++ systems/makanek/gitea.nix
@@ -1,9 +1,12 @@
-{
+let
+  inherit (import <niveum/lib>) sshPort;
+in {
   services.gitea = {
     enable = true;
     disableRegistration = true;
     rootUrl = "https://code.kmein.de";
     appName = "code.kmein.de";
+    ssh.clonePort = sshPort;
   };
   services.nginx.virtualHosts."code.kmein.de" = {
     forceSSL = true;
