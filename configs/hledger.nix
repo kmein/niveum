f7d030ac0979c5a80b52f9e0ed9880ef4ac69e57
Kierán Meinhardt <kmein@posteo.de>
2022-05-17 22:34:18 +0200

    feat(hledger): add time tracking tools

--- configs/hledger.nix
+++ configs/hledger.nix
@@ -5,6 +5,17 @@
 }: {
   imports = [<niveum/modules/hledger.nix>];
 
+  environment.systemPackages = let
+    timeLedger = "$HOME/projects/ledger/time.timeclock";
+  in [
+    (pkgs.writers.writeDashBin "hora-edit" ''
+      $EDITOR + "${timeLedger}"
+    '')
+    (pkgs.writers.writeDashBin "hora" ''
+      ${pkgs.hledger}/bin/hledger -f "${timeLedger}" "$@"
+    '')
+  ];
+
   niveum.hledger = {
     enable = true;
     ledgerFile = "$HOME/projects/ledger/all.journal";
