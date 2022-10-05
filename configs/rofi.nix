df312dd6477aaf18a327779087da512ec4f0050c
Kierán Meinhardt <kmein@posteo.de>
2022-10-05 16:50:22 +0200

    fix(rofi-pass): include via home-manager
    also fix https://github.com/carnager/rofi-pass/issues/226

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -9,5 +9,13 @@ in {
     enable = true;
     font = "Monospace 10";
     theme = "${pkgs.rofi}/share/rofi/themes/Arc.rasi";
+    pass = {
+      enable = true;
+      extraConfig = ''
+        USERNAME_field='login'
+        help_color="#FF0000"
+      ''; # help_color set by https://github.com/mrossinek/dotfiles/commit/13fc5f24caa78c8f20547bf473266879507f13bf
+    };
+    plugins = [pkgs.rofi-calc];
   };
 }
