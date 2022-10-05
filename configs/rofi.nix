652b7d4852f7c35e50fba53b8f25f8118ab99e67
Kierán Meinhardt <kmein@posteo.de>
2022-10-05 17:09:34 +0200

    fix(rofi-pass): generate passwords, correct default user

--- configs/rofi.nix
+++ configs/rofi.nix
@@ -12,7 +12,12 @@ in {
     pass = {
       enable = true;
       extraConfig = ''
+        _pwgen() {
+          ${pkgs.genpass}/bin/genpass "$@"
+        }
+
         USERNAME_field='login'
+        default_user2=kmein
         help_color="#FF0000"
       ''; # help_color set by https://github.com/mrossinek/dotfiles/commit/13fc5f24caa78c8f20547bf473266879507f13bf
     };
