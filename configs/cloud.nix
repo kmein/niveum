9bb98f8bd0dfbeea5a5bb790ba135c490251f740
Kierán Meinhardt <kmein@posteo.de>
2022-10-25 09:18:08 +0200

    feat(nextcloud): sync fysi documents

--- configs/cloud.nix
+++ configs/cloud.nix
@@ -61,7 +61,8 @@ in {
       };
     in ''
       mkdir -p ${lib.escapeShellArg kieran.target}
-      ${pkgs.nextcloud-client}/bin/nextcloudcmd --user ${kieran.user} --password ${lib.escapeShellArg kieran.password} --path /Notes ${lib.escapeShellArg kieran.target} ${kieran.endpoint}
+      ${pkgs.nextcloud-client}/bin/nextcloudcmd --non-interactive --user ${kieran.user} --password ${lib.escapeShellArg kieran.password} --path /Notes ${lib.escapeShellArg kieran.target} ${kieran.endpoint}
+      ${pkgs.nextcloud-client}/bin/nextcloudcmd --non-interactive --user ${kieran.user} --password ${lib.escapeShellArg kieran.password} --path /kmein ${lib.escapeShellArg "${config.users.users.me.home}/cloud/Seafile/Documents/Akten/FYSI/Lohn"} ${kieran.endpoint}
     '';
     serviceConfig = {
       Type = "oneshot";
