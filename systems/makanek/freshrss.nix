cae4423fb30fa66032acbe048cbcbb5600c9dac1
Kierán Meinhardt <kmein@posteo.de>
2022-11-16 10:03:46 +0100

    feat(freshrss): run from unstable, use with newsboat

--- systems/makanek/freshrss.nix
+++ systems/makanek/freshrss.nix
@@ -1,13 +1,35 @@
-let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   domain = "feed.kmein.de";
 in {
+  imports = [<nixos-unstable/nixos/modules/services/web-apps/freshrss.nix>];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    freshrss = (import <nixos-unstable> {}).freshrss;
+  };
+
+  # services.tt-rss = {
+  #   enable = true;
+  #   logDestination = "syslog";
+  #   root = "/var/lib/tt-rss";
+  #   selfUrlPath = "https://${domain}";
+  #   virtualHost = domain;
+  #   registration = {
+  #     enable = true;
+  #     maxUsers = 3;
+  #   };
+  # };
+
   services.freshrss = {
     enable = true;
     baseUrl = "https://${domain}";
     dataDir = "/var/lib/freshrss";
     database.type = "sqlite";
     defaultUser = "k";
-    passwordFile = toString <secrets/freshrss/password>;
+    passwordFile = pkgs.writeText "freshrss-password" (lib.strings.fileContents <secrets/freshrss/password>);
     virtualHost = domain;
   };
 
