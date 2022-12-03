e1e48ce43af74cb960d76b375fef1597cc976d7b
Kierán Meinhardt <kmein@posteo.de>
2022-12-03 11:34:33 +0100

    feat(nix): auto gc for saving space

--- configs/save-space.nix
+++ configs/save-space.nix
@@ -8,4 +8,9 @@
   nix.gc.automatic = true;
   nix.optimise.automatic = true;
   services.journald.extraConfig = "SystemMaxUse=500M";
+  # https://webzine.snowflake.ovh/issue-001.html -- garbage collect when less then 1 GB is available, make 3 GB fee
+  nix.extraOptions = ''
+    min-free = 1073741824
+    max-free = 3221225472
+  '';
 }
