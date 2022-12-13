e4514389f1acc1e675e40bb415a8fc4ef5a01184
Kierán Meinhardt <kmein@posteo.de>
2022-12-13 14:12:40 +0100

    feat(color): use papercolor

---
+++ lib/colours/papercolor-dark.nix
@@ -0,0 +1,37 @@
+rec {
+  black = {
+    dark = "#1c1c1c";
+    bright = "#585858";
+  };
+  red = {
+    dark = "#af005f";
+    bright = "#5faf5f";
+  };
+  green = {
+    bright = "#afd700";
+    dark = "#5faf00";
+  };
+  yellow = {
+    bright = "#af87d7";
+    dark = "#d7af5f";
+  };
+  blue = {
+    dark = "#5fafd7";
+    bright = "#ffaf00";
+  };
+  magenta = {
+    bright = "#ff5faf";
+    dark = "#808080";
+  };
+  cyan = {
+    dark = "#d7875f";
+    bright = "#00afaf";
+  };
+  white = {
+    dark = "#d0d0d0";
+    bright = "#5f8787";
+  };
+  background = black.dark;
+  foreground = white.dark;
+  cursor = blue.bright;
+}
