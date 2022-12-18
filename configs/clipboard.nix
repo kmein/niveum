97d26624c61b4239a43b9f9dbfc18436737bd88c
Kierán Meinhardt <kmein@posteo.de>
2022-12-18 13:51:22 +0100

    feat(autocutsel): disable because i dont want to paste everything i select

--- configs/clipboard.nix
+++ configs/clipboard.nix
@@ -4,18 +4,4 @@
   ...
 }: {
   services.clipmenu.enable = true;
-
-  # synchronize all the clipboards
-  systemd.user.services.autocutsel = {
-    enable = true;
-    wantedBy = ["graphical-session.target"];
-    after = ["graphical-session.target"];
-    serviceConfig = {
-      Type = "forking";
-      ExecStart = pkgs.writers.writeDash "autocutsel" ''
-        ${pkgs.autocutsel}/bin/autocutsel -fork -selection PRIMARY
-        ${pkgs.autocutsel}/bin/autocutsel -fork -selection CLIPBOARD
-      '';
-    };
-  };
 }
