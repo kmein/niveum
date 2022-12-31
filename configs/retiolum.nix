1aa7b78b4acd0216416a54baaf92d382e3c5bd70
Kierán Meinhardt <kmein@posteo.de>
2022-12-31 18:54:46 +0100

    fix: use my keys for @Mic92 retiolum module

--- configs/retiolum.nix
+++ configs/retiolum.nix
@@ -8,4 +8,9 @@
   ];
 
   networking.hosts = {"42:0:ca48:f98f:63d7:31ce:922b:245d" = ["go"];};
+
+  services.tinc.networks.retiolum = {
+    rsaPrivateKeyFile = toString <system-secrets/retiolum.key>;
+    ed25519PrivateKeyFile = toString <system-secrets/retiolum.ed25519>;
+  };
 }
