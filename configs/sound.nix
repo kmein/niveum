a0ac0a4b18cd6d57f88a82608fa3c482f64e8f49
Kierán Meinhardt <kmein@posteo.de>
2023-01-06 15:32:20 +0100

    feat: pipewire

---
+++ configs/sound.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}: {
+  sound.enable = true;
+
+  # realtime audio for pulseaudio
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    systemWide = false;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+    jack.enable = true;
+
+    # config.pipewire-pulse = {};
+  };
+
+  hardware.pulseaudio = {
+    enable = false;
+    package = pkgs.pulseaudioFull;
+    # copy server:/run/pulse/.config/pulse/cookie to client:~/.config/pulse/cookie to authenticate a client machine
+    zeroconf.discovery.enable = true;
+    extraConfig = ''
+      load-module ${
+        toString [
+          "module-tunnel-sink-new"
+          "server=zaatar.r"
+          "sink_name=zaatar"
+          "channels=2"
+          "rate=44100"
+        ]
+      }
+    '';
+  };
+
+  users.users.me.extraGroups = ["pipewire" "audio"];
+
+  environment.systemPackages = [pkgs.pavucontrol pkgs.ncpamixer pkgs.pamixer pkgs.pulsemixer];
+}
