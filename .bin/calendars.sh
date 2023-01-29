735ee26c5b2bff431be97a08cc457b8b3ea57875
Kierán Meinhardt <kmein@posteo.de>
2023-01-29 19:20:51 +0100

    feat: calendar add more planets

--- .bin/calendars.sh
+++ .bin/calendars.sh
@@ -6,12 +6,12 @@ clean() {
   rm -rf "$directory"
 }
 
-year=2022
+year=$(date +%Y)
 output=/tmp/$year.pdf
 
 for month in $(seq 1 12); do
   printf "\r%d" "$month" 1>&2
-  astrolog -zN Berlin -qm "$month" "$year" -X -K -XA -R Uranus Neptune Pluto -Xr -Xm -Xb -Xo "$(printf "%s/%02d.bmp" "$directory" "$month")" -Xw 1080 720 2>/dev/null
+  astrolog -zN Berlin -qm "$month" "$year" -X -K -XA -Xr -Xm -Xb -Xo "$(printf "%s/%02d.bmp" "$directory" "$month")" -Xw 1080 720 2>/dev/null
 done
 printf "\r"
 
