b9b3620e3894804aad2a9420ccfe52a45bb0b0c8
Kierán Meinhardt <kmein@posteo.de>
2023-02-07 22:30:16 +0100

    feat(compton): fade + inactiveOpacity

--- configs/compton.nix
+++ configs/compton.nix
@@ -1,6 +1,10 @@
 {
-  services.compton = {
+  services.picom = {
     enable = true;
+    activeOpacity = 1;
+    fade = true;
+    fadeDelta = 10;
+    inactiveOpacity = 0.8;
     shadow = true;
     menuOpacity = 0.9;
     shadowOpacity = 0.3;
