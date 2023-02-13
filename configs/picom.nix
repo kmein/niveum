d0eb69229d4c665512c29ff50ccb7b572ece2b8f
Kierán Meinhardt <kmein@posteo.de>
2023-02-13 23:19:44 +0100

    feat(picom): make animations faster, rename module

--- configs/picom.nix
+++ configs/picom.nix
@@ -3,8 +3,8 @@
     enable = true;
     activeOpacity = 1;
     fade = true;
-    fadeDelta = 10;
-    inactiveOpacity = 0.8;
+    fadeDelta = 5;
+    inactiveOpacity = 0.9;
     shadow = true;
     menuOpacity = 0.9;
     shadowOpacity = 0.3;
