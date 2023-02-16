7f8b7b37a5bf1ec02f2a89770dce5250d1b69bb2
Kierán Meinhardt <kmein@posteo.de>
2023-02-16 08:46:18 +0100

    feat(ful): add matomo and backup

---
+++ systems/ful/matomo.nix
@@ -0,0 +1,42 @@
+{pkgs, ...}: let
+  inherit (import <niveum/lib>) kieran;
+in {
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = kieran.email;
+  };
+
+  services.matomo = {
+    enable = true;
+    hostname = "matomo.kmein.de";
+    nginx = {
+      serverName = "matomo.kmein.de";
+    };
+  };
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+    ensureDatabases = ["matomo"];
+    ensureUsers = [
+      {
+        name = "matomo";
+        ensurePermissions."matomo.*" = "ALL PRIVILEGES";
+      }
+    ];
+  };
+
+  services.mysqlBackup = {
+    enable = true;
+    databases = ["matomo"];
+  };
+}
