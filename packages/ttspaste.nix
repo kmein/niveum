031eee005535c39a2a07278ef59726e5e1da6e09
Kierán Meinhardt <kmein@posteo.de>
2023-02-28 20:23:46 +0100

    fix(ttspaste): use espeak

--- packages/ttspaste.nix
+++ packages/ttspaste.nix
@@ -1,9 +1,11 @@
 {
   writers,
   xclip,
-  curl,
-  mpv,
+  espeak,
 }:
 writers.writeDashBin "ttspaste" ''
-  ${xclip}/bin/xclip -selection clipboard -out | ${curl}/bin/curl -G http://tts.r/api/tts --data-urlencode 'text@-' | ${mpv}/bin/mpv -
+  ${xclip}/bin/xclip -selection clipboard -out | ${espeak}/bin/espeak
 ''
+# curl, mpv,
+# ${curl}/bin/curl -G http://tts.r/api/tts --data-urlencode 'text@-' | ${mpv}/bin/mpv -
+
