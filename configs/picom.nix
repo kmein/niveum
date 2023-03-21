eb5cedabf8bd121f23ba79258b568afcd3183837
Kierán Meinhardt <kmein@posteo.de>
2023-03-21 19:14:32 +0100

    fix(picom): do not make lockscreen transparent

--- configs/picom.nix
+++ configs/picom.nix
@@ -8,5 +8,14 @@
     shadow = true;
     menuOpacity = 0.9;
     shadowOpacity = 0.3;
+    opacityRules = [
+      # opacity-rule overrides both inactive and active opacity
+      "100:class_g = 'slock'" # don't want a transparent lock screen!
+
+      # video in browser tabs
+      # substring /regex match of title bar text
+      "100:name *?= 'Youtube'"
+      "100:name *?= 'slock'"
+    ];
   };
 }
