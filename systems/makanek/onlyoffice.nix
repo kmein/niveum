d7a68e3859714ff8814be014899bc8c0eca9b33c
Kierán Meinhardt <kmein@posteo.de>
2023-04-24 06:38:37 +0200

    fix(onlyoffice): make readable

--- systems/makanek/onlyoffice.nix
+++ systems/makanek/onlyoffice.nix
@@ -15,14 +15,12 @@
     owner = "onlyoffice";
   };
 
-  systemd.services.onlyoffice-docservice.serviceConfig.ExecStartPre = [
-    # otherwise this leads to nginx
-    # open() "/var/lib/onlyoffice/documentserver/App_Data/cache/files/data/conv_check_1138411943_docx/output.docx" failed (13: Permission denied)
-    # and mysterious 403 errors
-    (pkgs.writers.writeDash "make-reachable" ''
-      chmod a+x /var/lib/onlyoffice/documentserver/
-    '')
-  ];
+  # otherwise this leads to nginx
+  # open() "/var/lib/onlyoffice/documentserver/App_Data/cache/files/data/conv_check_1138411943_docx/output.docx" failed (13: Permission denied)
+  # and mysterious 403 errors
+  system.activationScripts.onlyoffice-readable.text = ''
+    chmod a+x /var/lib/onlyoffice/documentserver/
+  '';
 
   services.nginx.virtualHosts.${config.services.onlyoffice.hostname} = {
     enableACME = true;
