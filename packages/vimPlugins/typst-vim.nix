9f4da32cf41650b66d6f4588c41aaf40dee8c864
Kierán Meinhardt <kmein@posteo.de>
2023-05-07 19:36:08 +0200

    feat(vim): add typst plugin

---
+++ packages/vimPlugins/typst-vim.nix
@@ -0,0 +1,14 @@
+{
+  vimUtils,
+  fetchFromGitHub,
+}:
+vimUtils.buildVimPluginFrom2Nix {
+  pname = "typst.vim";
+  version = "2882f21";
+  src = fetchFromGitHub {
+    owner = "kaarmu";
+    repo = "typst.vim";
+    rev = "2882f211f1498c790bb857f8a912c8e86526a362";
+    sha256 = "0xr8k17ggqfdksf3kybimfl5djjz3h19k4479la06i5lnwvlhkh2";
+  };
+}
