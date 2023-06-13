e44460ca3c3901554f6bff0a5418e1f5f08ecd5f
Kierán Meinhardt <kmein@posteo.de>
2023-06-13 10:53:57 +0200

    feat: good bye channels

--- configs/nix.nix
+++ configs/nix.nix
@@ -1,9 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   nixpkgs = {
     config.allowUnfree = true;
   };
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   };
 }
