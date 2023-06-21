43b643d89339726ff17380f24e9d234d5302f12a
Kierán Meinhardt <kmein@posteo.de>
2023-06-21 10:41:06 +0200

    fix(gpt): use api key command arg

--- packages/gpt.nix
+++ packages/gpt.nix
@@ -9,7 +9,7 @@ writers.writeDashBin "gpt" ''
   json=$(jq --slurp --raw-input '{model:"${model}", messages: [{role: "user", content: .}]}')
   ${curl}/bin/curl -sSL https://api.openai.com/v1/chat/completions \
     -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $(pass api-keys/openai.com)" \
+    -H "Authorization: Bearer $(${apiKeyCommand})" \
     -d "$json" \
     | ${jq}/bin/jq -r '.choices[] | .message.content'
 ''
