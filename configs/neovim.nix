a8f4ca1efef4d3acb45d55a55dd29895dd5b9eb9
Kierán Meinhardt <kmein@posteo.de>
2023-06-22 21:00:51 +0200

    feat(neovim): export as package from flake

--- configs/neovim.nix
+++ configs/neovim.nix
@@ -10,65 +10,6 @@
 
   environment.systemPackages = [
     (pkgs.writers.writeDashBin "vim" ''neovim "$@"'')
-    (pkgs.neovim.override {
-      configure = {
-        customRC = ''
-          source ${../lib/vim/init.vim}
-
-          luafile ${../lib/vim/init.lua}
-        '';
-        packages.nvim = with pkgs.vimPlugins; {
-          start = [
-            ale
-            fzf-vim
-            fzfWrapper
-            supertab
-            undotree
-            tabular
-            # vimwiki
-            niveumPackages.vimPlugins-vim-colors-paramount
-            nvim-lspconfig
-            vim-commentary
-            vim-css-color
-            vim-eunuch
-            niveumPackages.vimPlugins-vim-fetch
-            vim-fugitive
-            vim-gitgutter
-            vim-repeat
-            vim-sensible
-            vim-surround
-            (pkgs.vimUtils.buildVimPlugin rec {
-              pname = "vim-dim";
-              version = "1.1.0";
-              name = "${pname}-${version}";
-              src = pkgs.fetchFromGitHub {
-                owner = "jeffkreeftmeijer";
-                repo = pname;
-                rev = version;
-                sha256 = "sha256-lyTZUgqUEEJRrzGo1FD8/t8KBioPrtB3MmGvPeEVI/g=";
-              };
-            })
-          ];
-          opt = [
-            csv
-            elm-vim
-            emmet-vim
-            haskell-vim
-            niveumPackages.vimPlugins-icalendar-vim
-            niveumPackages.vimPlugins-jq-vim
-            rust-vim
-            typescript-vim
-            vim-javascript
-            vim-ledger
-            vim-nix
-            vimtex
-            vim-pandoc
-            vim-pandoc-syntax
-            niveumPackages.vimPlugins-vim-256noir
-            niveumPackages.vimPlugins-typst-vim
-          ];
-        };
-      };
-    })
+    niveumPackages.vim
   ];
 }
