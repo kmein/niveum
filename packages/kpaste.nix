a7d1e59fbce431c19082dfd8d73c74b8e8e61386
Kierán Meinhardt <kmein@posteo.de>
2023-07-12 15:14:39 +0200

    fix(kpaste): also show non-retiolum address
    this also fixes panoptikon

--- packages/kpaste.nix
+++ packages/kpaste.nix
@@ -5,6 +5,6 @@
 }:
 writers.writeDashBin "kpaste" ''
   ${curl}/bin/curl -sS http://p.r --data-binary @"''${1:--}" \
-    -H "Content-Type-Override: ''${KPASTE_CONTENT_TYPE-}"
+    -H "Content-Type-Override: ''${KPASTE_CONTENT_TYPE-}" |
   ${gnused}/bin/sed '$ {p;s|http://p.r|https://p.krebsco.de|}'
 ''
