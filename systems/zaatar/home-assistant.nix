915695cb0be06cb288d38a992f46bcb5aac44ee2
Kierán Meinhardt <kmein@posteo.de>
2023-07-22 23:43:34 +0200

    feat(home-assistant): activate zigbee dongle

--- systems/zaatar/home-assistant.nix
+++ systems/zaatar/home-assistant.nix
@@ -26,12 +26,12 @@ in {
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
-      volumes = ["home-assistant:/config"];
+      volumes = ["home-assistant.bak:/config"];
       environment.TZ = "Europe/Berlin";
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [
         "--network=host"
-        # "--device=/dev/ttyUSB0:/dev/ttyACM0"  # Example, change this to match your own hardware
+        "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
       ];
     };
   };
