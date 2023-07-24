eb9399fa177881c66fb14d9097896c292d3e9891
Kierán Meinhardt <kmein@posteo.de>
2023-07-24 06:29:51 +0000

    feat(moto): tmux, hledger

--- systems/moto/configuration.nix
+++ systems/moto/configuration.nix
@@ -1,25 +1,41 @@
 {
-  config,
-  lib,
   pkgs,
+  niveumPackages,
   inputs,
+  ...
 }: let
   sshPort = 8022;
 in {
   environment.packages = with pkgs; [
-    vim
-    openssh
-    curl
-    findutils
+    nil
+    bzip2
     coreutils
-    utillinux
+    curl
+    diffutils
+    findutils
+    git
     gnugrep
+    gnupg
     gnused
     gnutar
+    gzip
+    hostname
     iproute2
-    git
-    which
+    man
+    openssh
     procps
+    tzdata
+    unzip
+    utillinux
+    vim
+    which
+    xz
+    zip
+    hledger
+    hledger-ui
+    niveumPackages.vim
+    gitAndTools.gh
+    ripgrep
     (pkgs.writers.writeDashBin "start-ssh" ''
       ${pkgs.openssh}/bin/sshd -f ${pkgs.writeText "sshd_config" ''
         HostKey /data/data/com.termux.nix/files/home/.ssh/ssh_host_rsa_key
@@ -28,14 +44,19 @@ in {
     '')
   ];
 
-  nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 
   home-manager = {
     useGlobalPkgs = true;
     backupFileExtension = "hm-bak";
     config = {pkgs, ...}: {
       home.stateVersion = "23.05";
+      programs.tmux.enable = true;
     };
+  };
+
+  environment.sessionVariables = {
+    LEDGER_FILE = "/data/data/com.termux.nix/files/home/src/ledger/privat.journal";
   };
 
   user.shell = "${pkgs.fish}/bin/fish";
