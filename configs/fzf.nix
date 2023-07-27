35fcfc26c3dcd167feeda61c6c059ac1818d5d1a
Kierán Meinhardt <kmein@posteo.de>
2023-07-27 14:39:26 +0200

    feat(fzf): configure via home-manager so stylix gets it

--- configs/fzf.nix
+++ configs/fzf.nix
@@ -1,28 +1,22 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  environment = {
-    systemPackages = [pkgs.fzf];
-    variables = rec {
-      FZF_DEFAULT_COMMAND = "${pkgs.fd}/bin/fd --type f --strip-cwd-prefix --follow --no-ignore-vcs --exclude .git";
-      FZF_DEFAULT_OPTS =
-        lib.escapeShellArgs ["--height=40%"];
-      FZF_ALT_C_COMMAND = "${pkgs.fd}/bin/fd --type d";
-      FZF_ALT_C_OPTS = lib.escapeShellArgs [
+{pkgs, ...}: {
+  programs.fzf = {
+    fuzzyCompletion = true;
+    keybindings = true;
+  };
+
+  home-manager.users.me = {
+    programs.fzf = rec {
+      enable = true;
+      defaultCommand = "${pkgs.fd}/bin/fd --type f --strip-cwd-prefix --follow --no-ignore-vcs --exclude .git";
+      defaultOptions = ["--height=40%"];
+      changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+      changeDirWidgetOptions = [
         "--preview='${pkgs.tree}/bin/tree -L 1 \"{}\"'"
         "--bind=space:toggle-preview"
         "--preview-window=hidden"
       ];
-      FZF_CTRL_T_COMMAND = FZF_DEFAULT_COMMAND;
-      FZF_CTRL_T_OPTS =
-        lib.escapeShellArgs ["--preview='head -$LINES {}'"];
+      fileWidgetCommand = defaultCommand;
+      fileWidgetOptions = ["--preview='head -$LINES {}'"];
     };
-  };
-
-  programs.fzf = {
-    fuzzyCompletion = true;
-    keybindings = true;
   };
 }
