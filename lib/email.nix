556487f7ddfeb9ca7dd589a6e87c8ebf75ec3286
Kierán Meinhardt <kmein@posteo.de>
2023-09-28 17:54:20 +0200

    feat: thunderbird

--- lib/email.nix
+++ lib/email.nix
@@ -1,5 +1,10 @@
-{
+rec {
+  thunderbirdProfile = "donnervogel";
   defaults = {
+    thunderbird = {
+      enable = true;
+      profiles = [thunderbirdProfile];
+    };
     aerc.enable = true;
     realName = "Kierán Meinhardt";
     folders.inbox = "INBOX";
