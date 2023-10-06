a7715b0ec99f1d0837ce63d976ca3a80ead9799b
Kierán Meinhardt <kmein@posteo.de>
2023-10-06 10:57:35 +0200

    feat(aerc): add pronouns

--- lib/email.nix
+++ lib/email.nix
@@ -1,5 +1,17 @@
 rec {
   thunderbirdProfile = "donnervogel";
+  pronouns = builtins.concatStringsSep " · " [
+    "er"
+    "he"
+    "is"
+    "οὗτος"
+    "هو"
+    "ⲛ̄ⲧⲟϥ"
+    "он"
+    "han"
+    "सः"
+    "huwwe"
+  ];
   defaults = {
     thunderbird = {
       enable = true;
