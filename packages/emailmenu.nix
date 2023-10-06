3f50c45bed80e1d26f34bd9017255b2c6d9e80f0
Kierán Meinhardt <kmein@posteo.de>
2023-10-06 12:29:11 +0200

    feat: emailmenu

---
+++ packages/emailmenu.nix
@@ -0,0 +1,18 @@
+{
+  writers,
+  lib,
+  coreutils,
+  dmenu,
+  gawk,
+  libnotify,
+  xclip,
+  khard,
+}:
+writers.writeDashBin "emailmenu" ''
+  history_file=$HOME/.cache/emailmenu
+  PATH=${lib.makeBinPath [coreutils dmenu gawk libnotify xclip]}
+  chosen=$(${khard}/bin/khard email --parsable | awk '!seen[$0]++' | dmenu -p unicode -1 -l 10 | tee --append "$history_file" | cut -f1)
+  [ "$chosen" != "" ] || exit
+  echo "$chosen" | tr -d '\n' | xclip -selection clipboard
+  notify-send --app-name="$(basename "$0")" "'$chosen' copied to clipboard." &
+''
