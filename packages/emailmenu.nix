229a48ac07607efcbb6c2d7e74e8d42d29d7b8d4
Kierán Meinhardt <kmein@posteo.de>
2023-10-17 12:34:36 +0200

    feat(emailmenu): case insensitive, correct prompt

--- packages/emailmenu.nix
+++ packages/emailmenu.nix
@@ -11,7 +11,7 @@
 writers.writeDashBin "emailmenu" ''
   history_file=$HOME/.cache/emailmenu
   PATH=${lib.makeBinPath [coreutils dmenu gawk libnotify xclip]}
-  chosen=$(${khard}/bin/khard email --parsable | awk '!seen[$0]++' | dmenu -p unicode -1 -l 10 | tee --append "$history_file" | cut -f1)
+  chosen=$(${khard}/bin/khard email --parsable | awk '!seen[$0]++' | dmenu -i -p 📧 -1 -l 10 | tee --append "$history_file" | cut -f1)
   [ "$chosen" != "" ] || exit
   echo "$chosen" | tr -d '\n' | xclip -selection clipboard
   notify-send --app-name="$(basename "$0")" "'$chosen' copied to clipboard." &
