d9c324c22d51c70780c2eaa950324de937e7fb2f
Kierán Meinhardt <kmein@posteo.de>
2023-12-08 19:01:09 +0100

    fix(aerc): i want to read email again

--- configs/aerc.nix
+++ configs/aerc.nix
@@ -251,15 +251,15 @@ in {
           no-attachment-warning = "(attach|attached|attachments?|anbei|Anhang|angehängt|beigefügt)";
         };
         filters = {
-          "text/plain" = "${pkgs.gawk}/bin/awk -f ${pkgs.aerc}/share/aerc/filters/colorize";
-          "text/calendar" = "${pkgs.gawk}/bin/awk -f ${pkgs.aerc}/share/aerc/filters/calendar";
-          "text/html" = "${pkgs.aerc}/share/aerc/filters/html"; # Requires w3m, dante
+          "text/plain" = "${pkgs.aerc}/libexec/aerc/filters/colorize";
+          "text/calendar" = "${pkgs.aerc}/libexec/aerc/filters/calendar";
+          "text/html" = "${pkgs.aerc}/libexec/aerc/filters/html"; # Requires w3m, dante
           # "text/html" =
           #   "${pkgs.aerc}/share/aerc/filters/html | ${pkgs.aerc}/share/aerc/filters/colorize";
           # "text/*" =
           #   ''${pkgs.bat}/bin/bat -fP --theme=ansi --file-name="$AERC_FILENAME "'';
-          "message/delivery-status" = "${pkgs.gawk}/bin/awk -f ${pkgs.aerc}/share/aerc/filters/colorize";
-          "message/rfc822" = "${pkgs.gawk}/bin/awk -f ${pkgs.aerc}/share/aerc/filters/colorize";
+          "message/delivery-status" = "${pkgs.aerc}/libexec/aerc/filters/colorize";
+          "message/rfc822" = "${pkgs.aerc}/libexec/aerc/filters/colorize";
           "application/x-sh" = "${pkgs.bat}/bin/bat -fP -l sh";
         };
         openers = let
