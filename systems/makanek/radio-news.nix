0c612eeb7a17930de100fec3ad0999b94a79dd6a
Kierán Meinhardt <kmein@posteo.de>
2024-02-06 15:03:36 +0100

    fix: disable stockholm radio news

--- systems/makanek/radio-news.nix
+++ systems/makanek/radio-news.nix
@@ -15,19 +15,4 @@ in {
       description = "supplies git history news to radio lassulus and lets you enter your own.";
     }
   ];
-
-  systemd.services.stockholm-history = {
-    startAt = "*:58:00";
-    script = ''
-      stockholm=$(mktemp -d)
-      trap clean EXIT
-      clean() {
-        rm -rf "$stockholm"
-      }
-      ${pkgs.git}/bin/git clone ${remote} "$stockholm"
-      ${pkgs.git}/bin/git --git-dir "$stockholm"/.git log --pretty='"%s" by %an, %ar.' --since "$(${pkgs.coreutils}/bin/date -d '1 hours ago')" \
-        | ${pkgs.jq}/bin/jq -R '{text: ., from: now | todateiso8601, to: (now + (60 * 60)) | todateiso8601}' \
-        | ${pkgs.curl}/bin/curl -Ssfd @- http://radio-news.r/
-    '';
-  };
 }
