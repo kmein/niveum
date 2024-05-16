0dc44bf4c5eca88b4cdcffaa1f5cd5fd4deb35c9
Kierán Meinhardt <kmein@posteo.de>
2024-05-16 22:44:00 +0200

    mpv-tv: pin playlists

--- packages/mpv-tv.nix
+++ packages/mpv-tv.nix
@@ -8,13 +8,13 @@
   '';
 
   live-tv = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8";
-    hash = "sha256-l8tBwdTWm7l4h/eIHlcflDTgvv6lGMs6Vt7eVMpNLlw=";
+    url = "https://raw.githubusercontent.com/Free-TV/IPTV/39a573d7a428ca1b2ffeec422751a01d37e59e94/playlist.m3u8";
+    hash = "sha256-GBJBJN1AwwtO8HYrD0y3/qPCiK48IXyjt93s6DF/7Yo=";
     postFetch = m3u-to-tsv;
   };
 
   kodi-tv = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/jnk22/kodinerds-iptv/master/iptv/kodi/kodi_tv.m3u";
+    url = "https://raw.githubusercontent.com/jnk22/kodinerds-iptv/3f35761b7edcfb356d22cac0e561592ba589c20b/iptv/kodi/kodi_tv.m3u";
     sha256 = "sha256-EZEshHWUejLTy6qsBhELfaYdDpQ/uqPsZa1JA0mb7h0=";
     postFetch = m3u-to-tsv;
   };
