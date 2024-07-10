6e82ef3b5af02e25f30a48258f31b4c5ac98a70d
Kierán Meinhardt <kmein@posteo.de>
2024-07-10 10:27:30 +0200

    move back to zaatar

--- systems/zaatar/hardware-configuration.nix
+++ systems/zaatar/hardware-configuration.nix
@@ -22,16 +22,18 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-label/nixos";
+      device = "/dev/disk/by-uuid/5dcaa7fe-08e7-46a6-ac93-b267eeb144eb";
       fsType = "ext4";
     };
     "/boot" = {
-      device = "/dev/disk/by-label/boot";
+      device = "/dev/disk/by-uuid/CE3B-F4C6";
       fsType = "vfat";
     };
   };
 
-  swapDevices = [{device = "/dev/disk/by-label/swap";}];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/7b2a3e4c-e53f-4c53-b599-b6d6cff49f1f"; }
+    ];
 
   nix.settings.max-jobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
