26241be9d2856360b4b99d1dbc1d072b989b14a6
Kierán Meinhardt <kmein@posteo.de>
2024-09-19 09:54:12 +0200

    noise generators

--- packages/noise-waves.nix
+++ packages/noise-waves.nix
@@ -1,7 +1,7 @@
 { sox, mpv, writers, coreutils }:
 # ref https://askubuntu.com/a/789472
 writers.writeDashBin "noise-waves" ''
-  file="/tmp/noise.wav"
+  file="/tmp/noise-$(${coreutils}/bin/date +%s | ${coreutils}/bin/md5sum | ${coreutils}/bin/cut -d' ' -f1).wav"
   trap clean EXIT
   clean() {
     rm "$file"
@@ -9,6 +9,9 @@ writers.writeDashBin "noise-waves" ''
 
   ${coreutils}/bin/mkfifo "$file"
 
-  ${sox}/bin/sox -n "$file" synth brownnoise synth pinknoise mix synth sine amod 0.3 10 &
+  ${sox}/bin/sox -c2 -r44.1k -b8 -n "$file" synth brownnoise synth pinknoise mix synth sine amod 0.3 10 &
   ${mpv}/bin/mpv "$file"
 ''
+
+# play -c2 -r44.1k -b8 -n synth -1 brownnoise .1 40
+# play -c2 -r44.1k -b8 -n synth -1 pinknoise .1 60
