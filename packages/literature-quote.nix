69d1fd44f8de60ccb7ddb95ffb24ee67c87e85b4
Kierán Meinhardt <kmein@posteo.de>
2024-10-08 18:44:54 +0200

    literature-quote: improve formatting

--- packages/literature-quote.nix
+++ packages/literature-quote.nix
@@ -8,12 +8,31 @@
 writers.writeDashBin "literature-quote" ''
   PATH=$PATH:${lib.makeBinPath [xsv curl gnused]}
   ROW=$(curl -Ls http://kmein.github.io/logotheca/quotes.csv | shuf -n1)
-
   (
-    printf '%s\n\n— %s: _%s_, %s\n' \
-      "$(echo "$ROW" | xsv select 4)" \
-      "$(echo "$ROW" | xsv select 1)" \
-      "$(echo "$ROW" | xsv select 2)" \
-      "$(echo "$ROW" | xsv select 3 | tr : ,)"
-  ) | sed 's/ | /\n/g;s/ || /\n\n/g;s/"\(.*\)"/\1/'
+    QUOTE="$(echo "$ROW" | xsv select 4)"
+
+    echo "$QUOTE" | sed 's/^"//;s/"$//;s/\s*||\s*/\n\n/g;s/\s*|\s*/\n/g'
+    echo
+
+    AUTHOR="$(echo "$ROW" | xsv select 1)"
+    # Prepare the output
+    ATTRIBUTION="($AUTHOR"
+
+    SOURCE="$(echo "$ROW" | xsv select 2)"
+
+    # Add SOURCE if it's not empty
+    if [ -n "$SOURCE" ]; then
+      ATTRIBUTION="$ATTRIBUTION: $SOURCE"
+    fi
+
+    LOC="$(echo "$ROW" | xsv select 3 | sed 's/""//;s/-/–/g')"
+    # Add LOC if it's not empty
+    if [ -n "$LOC" ]; then
+      ATTRIBUTION="$ATTRIBUTION, $LOC"
+    fi
+
+    ATTRIBUTION="$ATTRIBUTION)"
+
+    echo "$ATTRIBUTION"
+  )
 ''
