7d38f3a7186efadb7a57224aef1b3fa4e2c2a3b0
Kierán Meinhardt <kmein@posteo.de>
2024-10-11 09:36:39 +0200

    bots: program nietzschebot

---
+++ configs/bots/nietzsche.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  niveumPackages,
+  ...
+}: {
+  niveum.bots.nietzsche = {
+    enable = true;
+    time = "08:00";
+    mastodon = {
+      enable = true;
+      tokenFile = config.age.secrets.mastodon-token-nietzsche.path;
+      language = "de";
+    };
+    command = toString (pkgs.writers.writeBash "random-nietzsche" ''
+      set -efu
+      random_number=$(( ($RANDOM % 10) + 1 ))
+      if [ "$random_number" -eq 1 ]; then
+        ${niveumPackages.random-zeno}/bin/random-zeno "/Literatur/M/Nietzsche,+Friedrich"
+      else
+        ${niveumPackages.random-zeno}/bin/random-zeno "/Philosophie/M/Nietzsche,+Friedrich"
+      fi
+    '');
+  };
+
+  systemd.timers.bot-nietzsche.timerConfig.RandomizedDelaySec = "10h";
+
+  age.secrets = {
+    mastodon-token-nietzsche.file = ../../secrets/mastodon-token-nietzsche.age;
+  };
+}
