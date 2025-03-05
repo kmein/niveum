8404430e8491e7d4869db9770050615a9d8e1b48
Kierán Meinhardt <kmein@posteo.de>
2025-03-05 10:04:45 +0100

    mycelium host aliases for all

--- configs/mycelium.nix
+++ configs/mycelium.nix
@@ -1,7 +1,15 @@
-{ ... }:
+{ lib, ... }:
+let
+  myceliumAddresses = import ../lib/mycelium-network.nix;
+in
 {
   services.mycelium = {
     enable = true;
     openFirewall = true;
   };
+
+  networking.hosts = lib.mapAttrs' (name: address: {
+    name = address;
+    value = [ "${name}.m" ];
+  }) myceliumAddresses;
 }
