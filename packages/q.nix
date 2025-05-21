e42a1c277590cdf17efc54260f8eeab815f097e6
Kierán Meinhardt <kmein@posteo.de>
2025-05-21 10:45:44 +0200

    heavily simplify dashboard

---
+++ packages/q.nix
@@ -0,0 +1,9 @@
+{ writers, lib, todoman, khal, util-linux, wego, pass }:
+writers.writeDashBin "q" ''
+  export PATH=$PATH:${lib.makeBinPath [todoman khal util-linux wego pass]}
+  (todo list --due 240; echo) &
+  (khal list today today; echo) &
+  (cal -3; echo) &
+  (wego -location Berlin -owm-api-key "$(pass api-keys/openweathermap)" -frontend emoji -days 2; echo) &
+  wait
+''
