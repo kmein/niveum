4e800bc171612e4ecbd39db71e3f11a672c8ec3c
Kierán Meinhardt <kmein@posteo.de>
2025-06-09 11:34:19 +0200

    literature-quote: fix selection with xan

--- packages/literature-quote.nix
+++ packages/literature-quote.nix
@@ -9,23 +9,23 @@ writers.writeDashBin "literature-quote" ''
   PATH=$PATH:${lib.makeBinPath [xan curl gnused]}
   ROW=$(curl -Ls http://kmein.github.io/logotheca/quotes.csv | shuf -n1)
   (
-    QUOTE="$(echo "$ROW" | xan select 4)"
+    QUOTE="$(echo "$ROW" | xan select 3)"
 
     echo "$QUOTE" | sed 's/^"//;s/"$//;s/\s*||\s*/\n\n/g;s/\s*|\s*/\n/g'
     echo
 
-    AUTHOR="$(echo "$ROW" | xan select 1)"
+    AUTHOR="$(echo "$ROW" | xan select 0)"
     # Prepare the output
     ATTRIBUTION="($AUTHOR"
 
-    SOURCE="$(echo "$ROW" | xan select 2)"
+    SOURCE="$(echo "$ROW" | xan select 1)"
 
     # Add SOURCE if it's not empty
     if [ -n "$SOURCE" ]; then
       ATTRIBUTION="$ATTRIBUTION: $SOURCE"
     fi
 
-    LOC="$(echo "$ROW" | xan select 3 | sed 's/""//;s/-/–/g')"
+    LOC="$(echo "$ROW" | xan select 2 | sed 's/""//;s/-/–/g')"
     # Add LOC if it's not empty
     if [ -n "$LOC" ]; then
       ATTRIBUTION="$ATTRIBUTION, $LOC"
