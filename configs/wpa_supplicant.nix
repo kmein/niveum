2f691caafaef95b5ec93e0427d35cfaf2c853146
Kierán Meinhardt <kmein@posteo.de>
2025-08-30 17:49:16 +0200

    change wifi name

--- configs/wpa_supplicant.nix
+++ configs/wpa_supplicant.nix
@@ -1,6 +1,9 @@
+{ config, ... }:
 {
   networking.wireless = {
     enable = true;
-    networks.Aether.pskRaw = "e1b18af54036c5c9a747fe681c6a694636d60a5f8450f7dec0d76bc93e2ec85a";
+    secretsFile = config.age.secrets.wifi.path;
+    # networks.Aether.pskRaw = "e1b18af54036c5c9a747fe681c6a694636d60a5f8450f7dec0d76bc93e2ec85a";
+    networks.Schilfpalast.pskRaw = "ext:schilfpalast";
   };
 }
