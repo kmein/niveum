a7a3b01676996c84a70346231ff1662569b47d39
Kierán Meinhardt <kmein@posteo.de>
2025-10-06 13:10:40 +0200

    host ical ephemeris MVP

--- systems/ful/ical-ephemeris.nix
+++ systems/ful/ical-ephemeris.nix
@@ -1,24 +1,24 @@
-{ config, pkgs, ... }:
+{ config, ... }:
+let
+  port = 9090;
+in
 {
   services.nginx.virtualHosts."ical-ephemeris.kmein.de" = {
-    addSSL = true;
+    forceSSL = true;
     enableACME = true;
     locations."/" = {
-      proxyPass = "http://localhost:9090";
+      proxyPass = "http://localhost:${toString port}/";
     };
   };
 
   services.ical-ephemeris = {
     enable = true;
-    port = 9090;
+    inherit port;
     brevoApiKeyFile = config.age.secrets.brevo-key.path;
     baseUrl = "https://ical-ephemeris.kmein.de";
   };
 
   age.secrets.brevo-key = {
     file = ../../secrets/brevo-key.age;
-    owner = "ical-ephemeris";
-    group = "ical-ephemeris";
-    mode = "0400";
   };
 }
