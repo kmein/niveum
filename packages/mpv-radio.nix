c1ca5336c8aa48e6daf9a423a4955d1f6c739aa6
Kierán Meinhardt <kmein@posteo.de>
2025-12-25 14:22:03 +0100

    streams: move to package

--- packages/mpv-radio.nix
+++ packages/mpv-radio.nix
@@ -1,35 +1,28 @@
 {
-  writeText,
-  lib,
   writers,
   mpv,
   dmenu,
   coreutils,
   gnused,
   di-fm-key-file,
+  radioStreams,
   executableName ? "mpv-radio",
-  mpvCommand ? "${mpv}/bin/mpv --force-window=yes"
-}: let
-  streams = import ../lib/streams.nix {
+  mpvCommand ? "${mpv}/bin/mpv --force-window=yes",
+}:
+let
+  streams = radioStreams.override {
     di-fm-key = "%DI_FM_KEY%";
   };
-  streams-tsv = writeText "streams.tsv" (lib.concatMapStringsSep "\n" ({
-    desc ? "",
-    stream,
-    station,
-    ...
-  }: "${station}\t${desc}\t${stream}")
-  streams);
 in
-  writers.writeDashBin executableName ''
-    set -x
+writers.writeDashBin executableName ''
+  set -x
 
-    if [ -z ''${DI_FM_KEY} ]; then
-      DI_FM_KEY=$(cat "${di-fm-key-file}")
-    fi
-    exec ${mpvCommand} "$(
-      ${dmenu}/bin/dmenu -i -l 5 < ${streams-tsv} \
-        | ${coreutils}/bin/cut -f3 \
-        | ${gnused}/bin/sed s/%DI_FM_KEY%/"$DI_FM_KEY"/
-    )"
-  ''
+  if [ -z ''${DI_FM_KEY} ]; then
+    DI_FM_KEY=$(cat "${di-fm-key-file}")
+  fi
+  exec ${mpvCommand} "$(
+    ${dmenu}/bin/dmenu -i -l 5 < ${streams.tsv} \
+      | ${coreutils}/bin/cut -f3 \
+      | ${gnused}/bin/sed s/%DI_FM_KEY%/"$DI_FM_KEY"/
+  )"
+''
--- packages/mpv-tuner.nix
+++ packages/mpv-tuner.nix
@@ -5,18 +5,19 @@
   mpv,
   gnused,
   di-fm-key-file,
+  radioStreams,
   findutils,
-}: let
-  streams = import ../lib/streams.nix {
+}:
+let
+  streams = radioStreams.override {
     di-fm-key = "%DI_FM_KEY%";
   };
-  streams-list = writeText "streams.txt" (lib.concatMapStringsSep "\n" (station: station.stream) streams);
 in
-  writers.writeDashBin "mpv-tuner" ''
-    if [ -z ''${DI_FM_KEY} ]; then
-      DI_FM_KEY=$(cat "${di-fm-key-file}")
-    fi
-    shuf ${streams-list} \
-      | ${gnused}/bin/sed s/%DI_FM_KEY%/"$DI_FM_KEY"/ \
-      | ${findutils}/bin/xargs ${mpv}/bin/mpv
-  ''
+writers.writeDashBin "mpv-tuner" ''
+  if [ -z ''${DI_FM_KEY} ]; then
+    DI_FM_KEY=$(cat "${di-fm-key-file}")
+  fi
+  shuf ${streams.playlist} \
+    | ${gnused}/bin/sed s/%DI_FM_KEY%/"$DI_FM_KEY"/ \
+    | ${findutils}/bin/xargs ${mpv}/bin/mpv
+''
