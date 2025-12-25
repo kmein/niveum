936ae927b79ea438d11319c5a1dab0c32bc96bf4
Kierán Meinhardt <kmein@posteo.de>
2025-12-25 08:34:53 +0100

    do not import nixpkgs with lookup path

--- packages/jsesh.nix
+++ packages/jsesh.nix
@@ -1,7 +1,13 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.writers.writeDashBin "jsesh" ''
-  ${pkgs.jre}/bin/java -jar ${pkgs.fetchzip {
-    url = "https://github.com/rosmord/jsesh/releases/download/release-7.5.5/JSesh-7.5.5.zip";
-    sha256 = "1z7ln51cil9pypz855x9a8p9ip2aflvknh566wcaah1kmz3fp57r";
-  }}/lib/jseshAppli-7.5.5.jar
+{
+  writers,
+  jre,
+  fetchzip,
+}:
+writers.writeDashBin "jsesh" ''
+  ${jre}/bin/java -jar ${
+    fetchzip {
+      url = "https://github.com/rosmord/jsesh/releases/download/release-7.5.5/JSesh-7.5.5.zip";
+      sha256 = "1z7ln51cil9pypz855x9a8p9ip2aflvknh566wcaah1kmz3fp57r";
+    }
+  }/lib/jseshAppli-7.5.5.jar
 ''
