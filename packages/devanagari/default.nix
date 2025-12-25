0189e20a8a59a9dcef1ee78f92a3fa432ce4e540
Kierán Meinhardt <kmein@posteo.de>
2025-12-25 08:42:13 +0100

    filter src

--- packages/devanagari/default.nix
+++ packages/devanagari/default.nix
@@ -1,7 +1,13 @@
-{yarn2nix-moretea}:
+{yarn2nix-moretea, lib}:
 yarn2nix-moretea.mkYarnPackage {
   name = "devanagari";
-  src = ./.;
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./devanagari.js
+      ./package.json
+    ];
+  };
   packageJson = ./package.json;
   yarnLock = ./yarn.lock;
 }
