f70383c732b3d782a262896ad64ebb10bfa5c5bb
Kierán Meinhardt <kmein@posteo.de>
2025-12-29 12:27:40 +0100

    tor: enable ssh

--- configs/tor.nix
+++ configs/tor.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  sshPort = pkgs.lib.niveum.machines.${config.networking.hostName}.sshPort;
+in
 {
   services.tor.enable = true;
   services.tor.client.enable = true;
@@ -6,4 +9,15 @@
     pkgs.tor
     pkgs.torsocks
   ];
+
+  services.tor.relay.onionServices = {
+    "ssh" = {
+      version = 3;
+      map = [{
+        port = sshPort;
+        target.port = sshPort;
+        target.addr = "127.0.0.1";
+      }];
+    };
+  };
 }
