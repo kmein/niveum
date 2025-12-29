1969ed2ad6c8ecd082b87e2dd7ccbfa10f8ccbef
Kierán Meinhardt <kmein@posteo.de>
2025-12-29 23:18:01 +0100

    gimp: fix

--- packages/gimp.nix
+++ packages/gimp.nix
@@ -10,18 +10,18 @@ let
     url = "https://c.krebsco.de/bring-out-the-gimp.png";
     hash = "sha256-k42M5j58OzbcMJwx9CeM2mD2VsVwPKHwJvU55+Rkowc=";
   };
-  data-dir-prefix = "share/gimp/2.0";
+  data-dir-prefix = "share/gimp/3.0";
   data-dir = symlinkJoin {
     name = "gimp";
     paths = [
       (runCommand "splash" { } ''
         mkdir -p $out/${data-dir-prefix}/images
-        install ${bring-out-the-gimp} $out/share/gimp/2.0/images/gimp-splash.png
+        install ${bring-out-the-gimp} ${placeholder "out"}/share/gimp/3.0/images/gimp-splash.png
       '')
       gimp
     ];
   };
 in
 writers.writeDashBin "gimp" ''
-  exec env GIMP2_DATADIR=${data-dir}/${data-dir-prefix} ${gimp}/bin/gimp "$@"
+  exec env GIMP3_DATADIR=${data-dir}/${data-dir-prefix} ${gimp}/bin/gimp "$@"
 ''
