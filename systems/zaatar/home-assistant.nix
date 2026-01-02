264cae96bd86d644a250db9da836b0c04053ecc4
Kierán Meinhardt <kmein@posteo.de>
2026-01-02 06:32:22 +0100

    wallabag: run on podman, autoupdate

--- systems/zaatar/home-assistant.nix
+++ systems/zaatar/home-assistant.nix
@@ -84,7 +84,7 @@ in
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [
         "--network=host"
-        "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
+        "--device=/dev/ttyACM0:/dev/ttyACM0"
       ];
     };
   };
