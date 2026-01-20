e389aa366dfa3b732d28cea92ba23f3a23d61815
Kierán Meinhardt <kmein@posteo.de>
2026-01-20 18:02:06 +0100

    waydroid

--- configs/virtualization.nix
+++ configs/virtualization.nix
@@ -1,14 +1,4 @@
 { pkgs, ... }:
 {
-  users.users.me.extraGroups = [ "libvirtd" ];
-  virtualisation.libvirtd.enable = true;
-
-  # Enable TPM support for VMs
-  virtualisation.libvirtd.qemu = {
-    # swtpm.enable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    virt-manager
-  ];
+  virtualisation.waydroid.enable = true;
 }
