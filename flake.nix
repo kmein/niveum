276ba28f716c56e2ce0d03311bf6e2081b2371ea
Kierán Meinhardt <kmein@posteo.de>
2026-05-01 10:02:18 +0200

    papercolor, iosevka

--- flake.nix
+++ flake.nix
@@ -434,7 +434,7 @@
                     }).wrapper;
                   niphas-editor = prev.niphas-editor.override {
                     withCopilot = true;
-                    colorscheme = "base16-gruvbox-light-medium";
+                    colorscheme = "base16-papercolor-light";
                   };
                 })
               ];
