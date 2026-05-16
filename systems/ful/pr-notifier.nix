7cfe5dfdba66f187738e63f4372eca1f9ef0bac3
Kierán Meinhardt <kmein@posteo.de>
2026-05-16 09:44:28 +0200

    pr-notifier

--- systems/ful/pr-notifier.nix
+++ systems/ful/pr-notifier.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 let
   port = 9505;
 in
@@ -6,10 +7,14 @@ in
     enable = true;
     inherit port;
     domain = "https://pr.kmein.de";
+    smtpCredentialsFile = config.age.secrets.pr-notifier-smtp.path;
+    smtpHost = "mail.cock.li";
+    githubTokenFile = config.age.secrets.pr-notifier-github.path;
+    smtpFrom = "sf5vpci6clp1qhbeu37rlir57s6n7lu8@airmail.cc";
+    emailFromName = "❄ PR Patrol";
   };
 
   services.nginx.virtualHosts."pr.kmein.de" = {
-    enable = true;
     enableACME = true;
     forceSSL = true;
     locations."/" = {
