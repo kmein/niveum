ddc8f9738c8b3697496e3bbe387d6110699f6766
Kierán Meinhardt <kmein@posteo.de>
2026-06-02 23:09:22 +0200

    Revert "remove tabula"
    This reverts commit 71ce12ff839db8288a2135bcf4503c84812ba880.

---
+++ systems/tabula/configuration.nix
@@ -0,0 +1,74 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../configs/spacetime.nix
+    ../../configs/retiolum.nix
+    ../../configs/sshd.nix
+  ];
+
+  age.secrets = {
+    retiolum-rsa = {
+      file = ../../secrets/tabula-retiolum-privateKey-rsa.age;
+      mode = "400";
+      owner = "tinc-retiolum";
+      group = "tinc-retiolum";
+    };
+    retiolum-ed25519 = {
+      file = ../../secrets/tabula-retiolum-privateKey-ed25519.age;
+      mode = "400";
+      owner = "tinc-retiolum";
+      group = "tinc-retiolum";
+    };
+  };
+
+  services.libinput.enable = true;
+
+  users.users.xenos = {
+    name = "xenos";
+    password = "xenos";
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" ];
+  };
+
+  services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = "xenos";
+    };
+  };
+
+  services.xserver = {
+    enable = true;
+    desktopManager.lxqt.enable = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.libreoffice
+    pkgs.gimp
+    pkgs.inkscape
+    pkgs.firefox
+    pkgs.pidgin
+    pkgs.git
+    pkgs.vim
+  ];
+
+  networking = {
+    useDHCP = false;
+    interfaces = {
+      enp0s4.useDHCP = true;
+      wlp2s0.useDHCP = true;
+    };
+    retiolum = pkgs.lib.niveum.retiolumAddresses.tabula;
+    hostName = "tabula";
+  };
+
+  networking.networkmanager.enable = true;
+
+  system.stateVersion = "21.11";
+}
